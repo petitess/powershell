fd2cdf86b7f837580e45162d70d0e5c3559e9536
petitess <93782725+petitess@users.noreply.github.com>
2021-11-16 08:38:47 +0100

    Update InstallAD_CreateDomain.ps1

--- InstallAD_CreateDomain.ps1
+++ InstallAD_CreateDomain.ps1
@@ -3,7 +3,7 @@ $DSRMPWord = ConvertTo-SecureString -String "12345.abc" -AsPlainText -Force
 #Write-Verbose "Installing Active Directory Services on this server" -Verbose
     Install-WindowsFeature -Name "AD-Domain-Services" -IncludeManagementTools
     #Write-Verbose "Configuring New Domain with on this server" -Verbose
-    Install-ADDSForest -ForestMode WinThreshold -DomainMode WinThreshold -DomainName "karol.se" `
+    Install-ADDSForest -ForestMode WinThreshold -DomainMode WinThreshold -DomainName "b3.test" `
     -InstallDns -NoDNSonNetwork -SafeModeAdministratorPassword $DSRMPWord -Force -NoRebootOnCompletion
     Restart-Computer -Force
 
