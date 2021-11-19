508ed45b589572d0b29fc08ae1e203bd1ae68246
petitess <93782725+petitess@users.noreply.github.com>
2021-11-19 14:23:42 +0100

    Update JoinDomain.ps1

--- JoinDomain.ps1
+++ JoinDomain.ps1
@@ -2,6 +2,6 @@ $DomainUser = "b3care.test\sek"
 $DomainPWord = ConvertTo-SecureString -String "12345678.abc" -AsPlainText -Force
 $DomainCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $DomainUser, $DomainPWord
 
-Add-Computer -ComputerName WEB01 -DomainName b3care.test -Credential $DomainCredential
+Add-Computer -DomainName b3care.test -Credential $DomainCredential
 
 Restart-Computer
