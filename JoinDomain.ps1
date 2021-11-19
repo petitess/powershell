144231bca59cee2c2afcaf11c0f9da110629f2f0
petitess <93782725+petitess@users.noreply.github.com>
2021-11-19 13:25:24 +0100

    JoinDomain.ps1

---
+++ JoinDomain.ps1
@@ -0,0 +1,7 @@
+$DomainUser = "b3care.test\sek"
+$DomainPWord = ConvertTo-SecureString -String "12345678.abc" -AsPlainText -Force
+$DomainCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $DomainUser, $DomainPWord
+
+Add-Computer -ComputerName WEB01 -DomainName b3care.test -Credential $DomainCredential
+
+Restart-Computer -Confirm:$true
