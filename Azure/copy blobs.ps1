8df0b2116636b991f37f55e38fa71e73e235b5f9
petitess <93782725+petitess@users.noreply.github.com>
2021-12-03 14:17:37 +0100

    Update copy blobs.ps1

--- Azure/copy blobs.ps1
+++ Azure/copy blobs.ps1
@@ -1,14 +1,14 @@
 ﻿
-$context = New-AzStorageContext -StorageAccountName "b3carenlbackupstorage" -StorageAccountKey "glKWUDoaKGV/Dk6WjsVznz0RurYBmtuqoMfTQGRPJ0POUBriuLSU1rS1V2KeY3jJvg6deH8CSIMyDKac6cfvhw=="
-#Container b3care-se-db06
+$context = New-AzStorageContext -StorageAccountName "backupstorage" -StorageAccountKey "LSU1rS1V2KeY3jJvg6deH8CSIMyDKac6cfvhw=="
+#Container se-db06
 
-$blobs = Get-AzStorageBlob -Container "b3care-se-db06" -Context $context 
+$blobs = Get-AzStorageBlob -Container "se-db06" -Context $context 
 $blobs | select name, lastmodified, versionid, length
 $files = ($blobs | Measure-Object | %{$_.count})
 $containersize = ($blobs | Measure-Object -Sum Length).Sum / 1GB
 Write-Output "    Container Size: $containersize (Gbytes) and $files files"
 ###COPY TO ANOTHER CONTAINER
-$newcontainer = "b3care-se-db06-backup"
+$newcontainer = "se-db06-backup"
 
 $blobs | Start-AzStorageBlobCopy -DestContainer $newcontainer -DestContext $context
 
