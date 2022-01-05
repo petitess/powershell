7f5e5a80b3d4df25d2293780a280c03d349cb119
petitess <93782725+petitess@users.noreply.github.com>
2022-01-05 14:12:53 +0100

    Update NewFile.ps1

--- NewFile.ps1
+++ NewFile.ps1
@@ -1,1 +1,4 @@
+#Create a new file
 New-Item -Path C:\Users\sek\Desktop -ItemType File -Name myfile.txt
+#Create a new file with content
+Set-Content -Path "C:\inetpub\wwwroot\Default.html" -Value "This is the server $($env:computername) !"
