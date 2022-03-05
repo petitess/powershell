49c2ac2ff4c378787d9970fae0f35c173f80cff0
petitess <93782725+petitess@users.noreply.github.com>
2022-03-05 12:49:44 +0100

    Add files via upload

---
+++ Azure/SubscriptionDeployment.ps1
@@ -0,0 +1,7 @@
+﻿Connect-AzAccount
+
+Get-AzSubscription
+
+Set-Location "C:\Users\Karol\OneDrive - B3 Consulting Group AB\Enviroment"
+
+New-AzSubscriptionDeployment -TemplateFile main.bicep -Location "swedencentral" -Name KarolDeploy
