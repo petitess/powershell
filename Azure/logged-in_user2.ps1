bfcfcba0164c368dc14a011a1300ec629df9da55
petitess <93782725+petitess@users.noreply.github.com>
2022-03-15 14:22:57 +0100

    Update logged-in_user2.ps1

--- Azure/logged-in_user2.ps1
+++ Azure/logged-in_user2.ps1
@@ -5,3 +5,5 @@ Get-WmiObject –ComputerName $env:COMPUTERNAME –Class Win32_ComputerSystem | 
 (Get-CimInstance -ClassName Win32_ComputerSystem).Username
 
 ((Get-WMIObject -ClassName Win32_ComputerSystem).Username).Split('\')[1]
+#Works with "Run command"
+Get-ChildItem Env:\USERNAME
