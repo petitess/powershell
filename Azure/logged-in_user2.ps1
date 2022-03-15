e478dfdfabd2604ce6cac9d7cffbd35ab81fa6c8
petitess <93782725+petitess@users.noreply.github.com>
2022-03-15 14:19:26 +0100

    Create logged-in_user2.ps1

---
+++ Azure/logged-in_user2.ps1
@@ -0,0 +1,7 @@
+Get-WmiObject –ComputerName $env:COMPUTERNAME –Class Win32_ComputerSystem | Select-Object UserName
+
+(Get-WMIObject -ClassName Win32_ComputerSystem).Username
+
+(Get-CimInstance -ClassName Win32_ComputerSystem).Username
+
+((Get-WMIObject -ClassName Win32_ComputerSystem).Username).Split('\')[1]
