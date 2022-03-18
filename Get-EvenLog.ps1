4fdc3f961e39ef59766f9f8d55e6188c25a864cc
Karol <93782725+petitess@users.noreply.github.com>
2022-03-18 15:13:17 +0100

    Update Get-EvenLog.ps1

--- Get-EvenLog.ps1
+++ Get-EvenLog.ps1
@@ -6,3 +6,5 @@ Get-EventLog -LogName Application -After $Date | Where -FilterScript {$_.EventID
 $Date = [DateTime]::Now.AddDays(-1) 
 Get-EventLog -LogName "Security" -After $Date | Where -FilterScript {$_.EventID -eq 4624}      
 
+#
+Get-WinEvent -FilterHashtable @{LogName = 'Security'; ID = 4624} -MaxEvents 10
