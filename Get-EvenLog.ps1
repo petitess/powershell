ee316cb2665d0c900656d1612940604767ab5ef6
Karol <93782725+petitess@users.noreply.github.com>
2022-03-18 15:12:17 +0100

    Create Get-EvenLog.ps1

---
+++ Get-EvenLog.ps1
@@ -0,0 +1,8 @@
+#
+$DatGete = [DateTime]::Now.AddDays(-100) 
+Get-EventLog -LogName Application -After $Date | Where -FilterScript {$_.EventID -eq 11707}  
+
+#
+$Date = [DateTime]::Now.AddDays(-1) 
+Get-EventLog -LogName "Security" -After $Date | Where -FilterScript {$_.EventID -eq 4624}      
+
