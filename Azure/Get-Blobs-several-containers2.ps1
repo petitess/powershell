242ad2832464014c6dd7271d533c7af72240a088
Karol <93782725+petitess@users.noreply.github.com>
2022-04-07 12:08:54 +0200

    Update Get-Blobs-several-containers2.ps1

--- Azure/Get-Blobs-several-containers2.ps1
+++ Azure/Get-Blobs-several-containers2.ps1
@@ -1,10 +1,10 @@
 $context = New-AzStorageContext -StorageAccountName "backupstorage" -StorageAccountKey "glKWUDoaKGV/Dk6WjMyDKac6cfvhw=="
-
 $containers = Get-AzStorageContainer -Context $context
 
-foreach ($container in $containers) {
-
+$list = foreach ($container in $containers) {
 $blobs = Get-AzStorageBlob -Container $container.Name -Context $context | Where-Object {$_.Name -like "*2022_04_07*"}
-$blobs
-
+$blobs 
+$blobs | Out-File "C:\Users\KarolSek\Desktop\$container.Name.txt"
 }
+
+$list | Out-File C:\Users\KarolSek\Desktop\lista2.txt -Append
