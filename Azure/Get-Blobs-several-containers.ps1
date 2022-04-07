be0383b92c125d33aca0873e2042dfb7b9047e67
Karol <93782725+petitess@users.noreply.github.com>
2022-04-07 10:46:31 +0200

    Update Get-Blobs-several-containers.ps1

--- Azure/Get-Blobs-several-containers.ps1
+++ Azure/Get-Blobs-several-containers.ps1
@@ -1,14 +1,13 @@
-param($Timer)
-$lifecycle = [DateTime]::UtcNow.AddDays(-1)     # 15 day
-
+#Gets blobs from multiple containers and send them to a file
 $context = New-AzStorageContext -StorageAccountName "backupstorage" -StorageAccountKey "glKWUDoaKGV/eY3jJvg6deH8CSIMyDKac6cfvhw=="
-Get-AzStorageBlob -Container "se-db01" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
-Get-AzStorageBlob -Container "se-db02" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
-Get-AzStorageBlob -Container "se-db03" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
-Get-AzStorageBlob -Container "se-db04" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
-Get-AzStorageBlob -Container "se-db06" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
-Get-AzStorageBlob -Container "se-db07" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
-Get-AzStorageBlob -Container "se-db09" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
-Get-AzStorageBlob -Container "se-db12" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
-Get-AzStorageBlob -Container "se-db13" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
+$blob1 = Get-AzStorageBlob -Container "se-db01" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
+$blob2 = Get-AzStorageBlob -Container "se-db02" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
+$blob3 = Get-AzStorageBlob -Container "se-db03" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
+$blob4 = Get-AzStorageBlob -Container "se-db04" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
+$blob5 = Get-AzStorageBlob -Container "se-db06" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
+$blob6 = Get-AzStorageBlob -Container "se-db07" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
+$blob7 = Get-AzStorageBlob -Container "se-db09" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
+$blob8 = Get-AzStorageBlob -Container "se-db12" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
+$blob9 = Get-AzStorageBlob -Container "se-db13" -Context $context | Where-Object {$_.Name -like "*2022_04_07*"} | select Name, LastModified
 
+$blob1,$blob2,$blob3,$blob4,$blob5,$blob5,$blob6,$blob7,$blob8,$blob9 | Out-File C:\Users\KarolSek\Desktop\lista.txt
