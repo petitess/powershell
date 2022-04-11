c981f0019e90a3a02a14aa26e4ae5b7fe5d85862
Karol <93782725+petitess@users.noreply.github.com>
2022-04-11 09:23:03 +0200

    Update Replace_result.ps1

--- Replace_result.ps1
+++ Replace_result.ps1
@@ -1,1 +1,20 @@
+$result = Get-AzAutomationSoftwareUpdateMachineRun -ResourceGroupName B3CARE-NL-NET -AutomationAccountName B3CARE-NL-AUTOMATION  | select TargetComputer
 
+$hash = @{}
+ $hash.'{' = ''
+ $hash."'" = ""
+ $hash.'}' = ''
+ #$hash.'[' = ''
+ $hash.']' = ''
+ $hash.'"' = ''
+ $hash.'TargetComputer=/subscriptions/xxxxfdb8-ba53-409c-xxxx-9118f13f6470/resourceGroups/' = ''
+ $hash.'TargetComputer=' = ''
+ $hash.'@' = ''
+ $hash.'xxxx-nl-ad/providers/Microsoft.Compute/virtualMachines/' = ''
+ $hash.'xxxx-nl-mgt/providers/Microsoft.Compute/virtualMachines/' = ''
+
+
+ Foreach ($key in $hash.Keys) {
+    $result = $result -replace $key, $hash.$key
+ }
+ $result
