b98e08a56a4f9a2f621ac8bbec0fd020faa8acef
Karol <93782725+petitess@users.noreply.github.com>
2022-04-28 07:46:26 +0200

    Create Start-AzVM.ps1

---
+++ Azure/Start-AzVM.ps1
@@ -0,0 +1,23 @@
+#For automation account
+$ErrorActionPreference = 'Continue'
+$TagKey = "AutoShutdown"
+$TagValues = "GroupA", "GroupB"
+
+$Conn = Get-AutomationConnection -Name AzureRunAsConnection
+Add-AzAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationID $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+
+foreach ($TagValue in $TagValues) {
+  "`nStarting VMs with tag `"{0}:{1}`":`n" -f $TagKey, $TagValue
+  $VMs = Get-AzResource -TagName $TagKey -TagValue $TagValue | Where-Object { $_.ResourceType -eq "Microsoft.Compute/virtualMachines" }
+
+  foreach ($VM in $VMs) {
+    "{0} ({1})" -f $VM.Name, $VM.ResourceGroupName
+
+    Start-AzVM -Name $VM.Name -ResourceGroupName $VM.ResourceGroupName
+  }
+
+  if ($null -eq $VMs) {
+    "-"
+  }
+  ""
+}
