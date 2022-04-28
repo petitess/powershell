054afd8fa80d2fb0474839553f4e65eeed9d08b7
Karol <93782725+petitess@users.noreply.github.com>
2022-04-28 07:45:31 +0200

    Create Stop-AzVM.ps1

---
+++ Azure/Stop-AzVM.ps1
@@ -0,0 +1,23 @@
+#For automation account
+$ErrorActionPreference = 'Continue'
+$TagKey = "AutoShutdown"
+$TagValues = "GroupB", "GroupA"
+
+$Conn = Get-AutomationConnection -Name AzureRunAsConnection
+Add-AzAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationID $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+
+foreach ($TagValue in $TagValues) {
+    "`nStopping VMs with tag `"{0}:{1}`":`n" -f $TagKey, $TagValue
+    $VMs = Get-AzResource -TagName $TagKey -TagValue $TagValue | Where-Object { $_.ResourceType -eq "Microsoft.Compute/virtualMachines" }
+
+    foreach ($VM in $VMs) {
+        "{0} ({1})" -f $VM.Name, $VM.ResourceGroupName
+
+        Stop-AzVM -Name $VM.Name -ResourceGroupName  $VM.ResourceGroupName -Force
+    }
+
+    if ($null -eq $VMs) {
+        "-"
+    }
+    ""
+}
