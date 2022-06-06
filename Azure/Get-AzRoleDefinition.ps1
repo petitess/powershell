e3fb900c90e9eb6daf08f3bc028a7efd173eec4f
Karol <93782725+petitess@users.noreply.github.com>
2022-06-06 18:15:37 +0200

    Add files via upload

---
+++ Azure/Get-AzRoleDefinition.ps1
@@ -0,0 +1,5 @@
+﻿(Get-AzRoleDefinition "Virtual Machine Contributor").Actions
+
+(Get-AzRoleDefinition "Contributor").NotActions
+
+Get-AzSubscriptionDeployment | where {$_.DeploymentName -match "Deploy"} | select DeploymentName
