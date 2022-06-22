7b7aa76670855905e2003b0c6ee10bc9fb3dd384
Karol <93782725+petitess@users.noreply.github.com>
2022-06-22 11:50:08 +0200

    Convert folder content to a txt file.

--- Get Name.ps1
+++ Get Name.ps1
@@ -1,8 +1,6 @@
-﻿Get-ChildItem 'C:\Users\karol\Music\disco polo 1' | select name | Out-File C:\Users\karol\Music\DISCOPOLO.TXT
+Get-ChildItem 'C:\Users\karol\Music\disco polo 1' | select name | Out-File C:\Users\karol\Music\DISCOPOLO.TXT
 
 Get-ChildItem 'C:\Users\karol\Music\POLSKIE' | select name | Out-File C:\Users\karol\Music\POLSKIE.TXT
-
-
 
 ####NAME WITHOUT .MP3
 $name = Get-ChildItem 'C:\Users\karol\Music\disco polo 1' | select name
