aee661eb70f0916efb4d774ae4e7f7282b8c22f6
Karol <93782725+petitess@users.noreply.github.com>
2022-06-22 11:29:57 +0200

    Removes old object from Win AD

--- Azure/Move-ADObject_old.ps1
+++ Azure/Move-ADObject_old.ps1
@@ -1,5 +1,5 @@
-$oldusers = Get-ADUser -Filter {Enabled -eq $TRUE} -SearchBase "ou=User,ou=Accounts,dc=ad,dc=almi,dc=se" -Properties Name,SamAccountName,LastLogonDate | Where {($_.LastLogonDate -lt (Get-Date).AddDays(-180)) -and ($_.LastLogonDate -ne $NULL)}
+$oldusers = Get-ADUser -Filter {Enabled -eq $TRUE} -SearchBase "ou=User,ou=Accounts,dc=ad,dc=domain,dc=se" -Properties Name,SamAccountName,LastLogonDate | Where {($_.LastLogonDate -lt (Get-Date).AddDays(-180)) -and ($_.LastLogonDate -ne $NULL)}
 
 foreach ($user in $oldusers){
-    Move-ADObject -Identity $user -TargetPath "ou=Elysium,dc=ad,dc=almi,dc=se"
+    Move-ADObject -Identity $user -TargetPath "ou=Elysium,dc=ad,dc=domain,dc=se"
 }
