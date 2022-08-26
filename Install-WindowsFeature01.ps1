d9e86a3e36443b32c5c9a8039f2ed88dbe5abaa5
Karol <93782725+petitess@users.noreply.github.com>
2022-08-26 07:04:44 +0200

    Update Install-WindowsFeature01.ps1

--- Install-WindowsFeature01.ps1
+++ Install-WindowsFeature01.ps1
@@ -3,7 +3,7 @@ $DSRMPWord = ConvertTo-SecureString -String "12345678.abc" -AsPlainText -Force
 #Write-Verbose "Installing Active Directory Services on this server" -Verbose
     Install-WindowsFeature -Name "AD-Domain-Services" -IncludeManagementTools
     #Write-Verbose "Configuring New Domain with on this server" -Verbose
-    Install-ADDSForest -ForestMode WinThreshold -DomainMode WinThreshold -DomainName "b3care.test" `
+    Install-ADDSForest -ForestMode WinThreshold -DomainMode WinThreshold -DomainName "domain.my" `
     -InstallDns -NoDNSonNetwork -SafeModeAdministratorPassword $DSRMPWord -Force -NoRebootOnCompletion
     Restart-Computer -Force
 
