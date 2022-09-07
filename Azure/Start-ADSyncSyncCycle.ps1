e7e4d5f439dc9a5d92114b0584cf86d3e7d9c1b3
Karol <93782725+petitess@users.noreply.github.com>
2022-09-07 16:24:17 +0200

    AzureAD Connect Sync

--- Azure/Start-ADSyncSyncCycle.ps1
+++ Azure/Start-ADSyncSyncCycle.ps1
@@ -7,3 +7,5 @@ Start-ADSyncSyncCycle -PolicyType Delta
 Start-ADSyncSyncCycle -PolicyType Initial
 
 Get-ADSyncConnectorRunStatus
+
+https://docs.microsoft.com/en-us/azure/active-directory/hybrid/how-to-connect-sync-feature-scheduler#scheduler-and-installation-wizard
