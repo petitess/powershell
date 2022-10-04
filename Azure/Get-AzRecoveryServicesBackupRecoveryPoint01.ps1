f8aeabbc12341479768f90ca56dc891f5042ed3c
Karol <93782725+petitess@users.noreply.github.com>
2022-10-04 13:51:05 +0200

    Get Restore points for VMs

--- Azure/Get-AzRecoveryServicesBackupRecoveryPoint01.ps1
+++ Azure/Get-AzRecoveryServicesBackupRecoveryPoint01.ps1
@@ -1,9 +1,16 @@
+$rsvname = "rsv-infra-prod-01"
+$infrargname = "rg-infra-prod-sc-01"
+
+$vault = Get-AzRecoveryServicesVault -Name $rsvname -ResourceGroupName $infrargname
+Set-AzRecoveryServicesVaultContext -Vault $vault
+
+
 $backupcontainer = Get-AzRecoveryServicesBackupContainer `
-    -ContainerType "AzureVM" `
-    -FriendlyName vmdcprod01
+    -ContainerType "AzureVM" `
+    -FriendlyName vmdcprod01
 
 $item = Get-AzRecoveryServicesBackupItem `
-    -Container $backupcontainer `
-    -WorkloadType "AzureVM"
+    -Container $backupcontainer `
+    -WorkloadType "AzureVM"
 
-Get-AzRecoveryServicesBackupRecoveryPoint -Item $item -RecoveryPointId 7701367240882239739 | select RecoveryPointId, RecoveryPointTime
+Get-AzRecoveryServicesBackupRecoveryPoint -Item $item -RecoveryPointId 7701367240882239739
