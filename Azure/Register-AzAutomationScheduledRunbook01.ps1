058c0f6c46754af2e526feb0a0b7eeb5bd959858
Karol <93782725+petitess@users.noreply.github.com>
2022-10-06 12:42:29 +0200

    Update Register-AzAutomationScheduledRunbook01.ps1

--- Azure/Register-AzAutomationScheduledRunbook01.ps1
+++ Azure/Register-AzAutomationScheduledRunbook01.ps1
@@ -1,13 +1,14 @@
-﻿$rgname = "rg-infra-prod-sc-01"
+$rgname = "rg-infra-prod-sc-01"
 $aaname = "aa-infra-prod-01"
 $runname = "run-test01"
 $schename = "StopVM01"
-$starttime = "23:00"
-$guid = ((New-Guid).Guid).substring(0,8)
+$starttime = "12:45"
 
 $aa = Get-AzAutomationAccount -ResourceGroupName $rgname -AutomationAccountName $aaname
 
-$run = Get-AzAutomationRunbook -AutomationAccountName $aa.AutomationAccountName -ResourceGroupName $aa.ResourceGroupName | Where-Object {$_.AutomationAccountName -eq $runname}
+$run = Get-AzAutomationRunbook -AutomationAccountName $aa.AutomationAccountName -ResourceGroupName $aa.ResourceGroupName | Where-Object {$_.Name -eq $runname}
+
+$sche = Get-AzAutomationScheduledRunbook -AutomationAccountName $aa.AutomationAccountName -ResourceGroupName $aa.ResourceGroupName | Where-Object {$_.RunbookName -eq $runname}
 
 if ($null -eq $run) {
 New-AzAutomationRunbook -Name $runname -AutomationAccountName $aa.AutomationAccountName -Type PowerShell -ResourceGroupName $aa.ResourceGroupName
@@ -19,5 +20,7 @@ Publish-AzAutomationRunbook -Name $runname -ResourceGroupName $aa.ResourceGroupN
 
 New-AzAutomationSchedule -Name $schename -AutomationAccountName $aa.AutomationAccountName -ResourceGroupName $aa.ResourceGroupName -StartTime $starttime -DayInterval 1 -TimeZone "Europe/Berlin"
 
-
+if ($null -eq $sche) {
 Register-AzAutomationScheduledRunbook -RunbookName $runname -ScheduleName $schename -AutomationAccountName $aa.AutomationAccountName -ResourceGroupName $aa.ResourceGroupName
+}
+
