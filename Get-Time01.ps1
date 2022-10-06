7e317869ea8dc9bc714f700227eea185186231d2
Karol <93782725+petitess@users.noreply.github.com>
2022-10-06 14:59:37 +0200

    If Else Statement

---
+++ Get-Time01.ps1
@@ -0,0 +1,33 @@
+﻿Get-Date
+
+Get-Date -DisplayHint Date
+
+Get-Date -Format "dddd MM/dd/yyyy HH:mm K"
+
+Get-Date -Format "MM/dd/yyyy"
+
+Get-Date -UFormat "%m/%d/%Y %R"
+
+$Temp = (Get-date).AddDays(1)
+Get-Date $Temp -UFormat "%m/%d/%Y %R"
+
+$time = "20:30"
+$Temp = (Get-date).AddDays(1)
+$x = Get-Date $Temp -UFormat "%m/%d/%Y $time"
+
+$now = Get-Date -UFormat "%R"
+if ($now -ge "15:55"){
+Write-Output "klockan har passerat 15:55"
+}else {
+Write-Output "klockan har inte passerat 15:55"
+}
+
+$now = Get-Date -UFormat "%R"
+if ($now -ge "15:55"){
+$time = "20:30"
+$Temp = (Get-date).AddDays(1)
+Get-Date $Temp -UFormat "%m/%d/%Y $time"
+}else {
+$time = "20:30"
+Get-Date -UFormat "%m/%d/%Y $time"
+}
