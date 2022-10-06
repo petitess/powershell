1d2695c20cd9420774de082d551c27233714bd77
Karol <93782725+petitess@users.noreply.github.com>
2022-10-06 16:12:47 +0200

    if else statement with time function

--- Azure/Register-AzAutomationScheduledRunbook02.ps1
+++ Azure/Register-AzAutomationScheduledRunbook02.ps1
@@ -11,19 +11,17 @@ $tomorrow = Get-Date $Temp -UFormat "%m/%d/%Y $time"
 $today = Get-Date -UFormat "%m/%d/%Y $time"
 
 $aa = Get-AzAutomationAccount -ResourceGroupName $rgname -AutomationAccountName $aaname
-
 $run = Get-AzAutomationRunbook -AutomationAccountName $aa.AutomationAccountName -ResourceGroupName $aa.ResourceGroupName | Where-Object {$_.Name -eq $runname}
-
 $sche = Get-AzAutomationScheduledRunbook -AutomationAccountName $aa.AutomationAccountName -ResourceGroupName $aa.ResourceGroupName | Where-Object {$_.RunbookName -eq $runname}
-
+#create a runbook
 if ($null -eq $run) {
 New-AzAutomationRunbook -Name $runname -AutomationAccountName $aa.AutomationAccountName -Type PowerShell -ResourceGroupName $aa.ResourceGroupName
 }
-
+#publish the runbook
 if ($null -eq $run) {
 Publish-AzAutomationRunbook -Name $runname -ResourceGroupName $aa.ResourceGroupName -AutomationAccountName $aa.AutomationAccountName
 }
-
+#create a schedule
 if ($null -eq $sche) {
 if ($now -ge "19:55"){
 New-AzAutomationSchedule -Name $schename -AutomationAccountName $aa.AutomationAccountName -ResourceGroupName $aa.ResourceGroupName -StartTime $tomorrow -DayInterval 1 -TimeZone "Europe/Berlin"
@@ -31,7 +29,7 @@ New-AzAutomationSchedule -Name $schename -AutomationAccountName $aa.AutomationAc
 New-AzAutomationSchedule -Name $schename -AutomationAccountName $aa.AutomationAccountName -ResourceGroupName $aa.ResourceGroupName -StartTime $today -DayInterval 1 -TimeZone "Europe/Berlin"
 }
 }
-
+#register the schedule with runbook
 if ($null -eq $sche) {
 Register-AzAutomationScheduledRunbook -RunbookName $runname -ScheduleName $schename -AutomationAccountName $aa.AutomationAccountName -ResourceGroupName $aa.ResourceGroupName
 }
