87cb406cc672146f3511b04042ecd53b99fe277e
Karol <93782725+petitess@users.noreply.github.com>
2022-11-03 09:27:15 +0100

    Get thumbprint of a certificate

---
+++ Get-Certificate01.ps1
@@ -0,0 +1,5 @@
+﻿New-SelfSignedCertificate –DnsName DESKTOP-GHENXXX.my.domain.com -CertStoreLocation “cert:\LocalMachine\My”
+
+$name = (Get-ComputerInfo).csname
+
+$thumprint = (Get-ChildItem -Path Cert:LocalMachine\MY | where {$_.Subject -eq "CN=$name.my.domain.com"}).Thumbprint
