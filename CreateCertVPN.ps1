449b5903736745c530169ac8f55c147add1b7b79
Karol <93782725+petitess@users.noreply.github.com>
2022-11-14 15:22:47 +0100

    Add files via upload

---
+++ CreateCertVPN.ps1
@@ -0,0 +1,62 @@
+﻿$CertLocation = 'C:\temp\VPN'
+$Cert = 'AndroidClient.pfx'
+$CertName = "$CertLocation$Cert"
+
+if((Test-Path -Path $CertLocation -ErrorAction SilentlyContinue) -eq $false){
+    mkdir $CertLocation
+    cd $CertLocation
+}
+else {
+    cd $CertLocation
+}
+$cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
+    -Subject "CN=AndroidRoot" `
+    -KeyExportPolicy Exportable `
+    -HashAlgorithm sha256 `
+    -KeyLength 2048 `
+    -CertStoreLocation "Cert:\CurrentUser\My" `
+    -KeyUsageProperty Sign -KeyUsage CertSign
+
+New-SelfSignedCertificate `
+    -Type Custom `
+    -DnsName P2SChildCert `
+    -KeySpec Signature `
+    -Subject "CN=AndroidClient" `
+    -KeyExportPolicy Exportable `
+    -HashAlgorithm sha256 `
+    -KeyLength 2048 `
+    -CertStoreLocation "Cert:\CurrentUser\My" `
+    -Signer $cert `
+    -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
+
+
+
+$RootCert = (Get-ChildItem `
+    -Path "Cert:\CurrentUser\My\"`
+    | Where-Object `
+    -Property subject `
+    -Match AndroidRoot)
+$ClientCert = (Get-ChildItem `
+    -Path "Cert:\CurrentUser\My\"`
+    | Where-Object `
+    -Property subject `
+    -Match AndroidClient)
+Export-Certificate `
+    -Type CERT `
+    -Cert $RootCert `
+    -FilePath "$CertLocation\AndroidRootTemp.cer"
+Export-Certificate `
+    -Type CERT `
+    -Cert $ClientCert `
+    -FilePath "$CertLocation\AndroidClient.cer"
+C:\windows\system32\certutil.exe -encode "$CertLocation\AndroidRootTemp.cer" 'AndroidRoot.cer'
+Get-Content $CertLocation\AndroidRoot.cer
+$SecurePassword = Read-Host `
+    -Prompt "Enter Password to Export Cert with Private Key" `
+    -AsSecureString
+$ThumbPrint = $ClientCert.Thumbprint
+$ExportPrivateCertPath = "Cert:\CurrentUser\My\$ThumbPrint"
+Export-PfxCertificate `
+    -FilePath "C:\temp\VPN\AndroidClient.pfx" `
+    -Password $SecurePassword `
+    -Cert $ExportPrivateCertPath
