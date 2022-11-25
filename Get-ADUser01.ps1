fe518c1ed6c53482cc9db9b6cdbbdc3fc901a44d
Karol <93782725+petitess@users.noreply.github.com>
2022-11-25 12:26:38 +0100

    Delete Get-ADUser01.ps1

--- Get-ADUser01.ps1
+++
@@ -1,8 +0,0 @@
-$OldCompanyName = "Company Old AB"
-$NewCompanyName = "Company New AB"
-$OU = "OU=User,OU=Accounts,DC=ad,DC=almi,DC=se"
-
-$Users = Get-ADUser -SearchBase $OU -properties Company -filter {Company -like $OldCompanyName}
-$Users | Foreach {
-         Set-ADUser -Identity $_ -Company $NewCompanyName
-     }
