ea7e5525d4de880743866d71b9ae7a087b4e00c6
Karol <93782725+petitess@users.noreply.github.com>
2022-11-26 09:26:45 +0100

    Get resources with tags and print to CSV

---
+++ Azure/Get-AzResource01.ps1
@@ -0,0 +1,24 @@
+﻿
+$subscripionList =  Get-AzSubscription 
+
+ ForEach ($subscriptionId in $subscripionList) {
+    Set-AzContext -Subscription $subscriptionId
+
+    $resources = Get-AzResource | Where-Object {$_.Tags.Values -eq "dev"}
+
+        foreach ($resource in $resources) {
+
+        $Res = $resource | select name, ResourceGroupName, SubscriptionId
+        Write-Host $resource.Name $resource.ResourceGroupName $resource.SubscriptionId
+        $Res | Export-Csv C:\Users\Karol\Desktop\Res$(Get-Date -Format 'yyyy-MM-dd').csv -Append -Encoding UTF8
+    }
+}
+
+#Compare files
+
+$File1 = Import-Csv -Path "C:\Users\$env:username\Desktop\Res$((get-date).AddDays(-1).ToString("yyy-MM-dd")).csv"
+$File2 = Import-Csv -Path "C:\Users\$env:username\Desktop\Res$(Get-Date -Format 'yyyy-MM-dd').csv"
+Write-Host "NYA RESURSER:"
+Compare-Object $File1 $File2 | select inputobject
+
+
