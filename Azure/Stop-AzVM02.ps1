aa72366e21b9320ec9c6e1320f541cd496dfdbb8
Karol <93782725+petitess@users.noreply.github.com>
2022-11-27 11:34:55 +0100

    Add files via upload

---
+++ Azure/Stop-AzVM02.ps1
@@ -0,0 +1,10 @@
+﻿$TagKey = "AutoShutdown"
+$TagValues = "Yes"
+
+$VMs = Get-AzVM | Where-Object {$_.Tags.Keys -eq $TagKey -and $_.Tags.Values -eq $TagValues}
+
+ForEach ($VM in $VMs) {
+
+Stop-AzVM -ResourceGroupName $VM.ResourceGroupName -Name $VM.Name -Force
+
+}
