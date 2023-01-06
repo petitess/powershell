473609c60acaf41ac01c525aca3156d70cbc97b2
Karol <93782725+petitess@users.noreply.github.com>
2023-01-06 12:18:27 +0100

    RSAT Windows Server

--- Install-WindowsFeature02.ps1
+++ Install-WindowsFeature02.ps1
@@ -1,1 +1,13 @@
 Install-WindowsFeature -IncludeAllSubFeature RSAT'
+
+Get-WindowsFeature | Where-Object {$_.Name -like 'RSAT*'} | select Name,DisplayName                               
+     
+Get-WindowsFeature -Name RSAT-AD-Tools  | Select-Object Name , InstallState  
+Get-WindowsFeature -Name RSAT-DHCP | Select-Object Name , InstallState        
+Get-WindowsFeature -Name RSAT-DNS-Server | Select-Object Name , InstallState 
+Get-WindowsFeature -Name RSAT-ADCS | Select-Object Name , InstallState           
+
+Install-WindowsFeature -Name RSAT-AD-Tools      
+Install-WindowsFeature -Name RSAT-DHCP 
+Install-WindowsFeature -Name RSAT-DNS-Server     
+Install-WindowsFeature -Name RSAT-ADCS 
