50268f2a48bb2a1e740acd6f4878dd168257fc4a
Karol <93782725+petitess@users.noreply.github.com>
2023-01-11 10:27:25 +0100

    ADsynck from mgmt srv

---
+++ AD/Start-ADSyncSyncCycle01.ps1
@@ -0,0 +1,5 @@
+$hostName="vmaadcprod01.ad.comp.se" 
+$winrmPort = "5986"
+$cred = Get-Credential
+$soptions = New-PSSessionOption -SkipCACheck
+Invoke-Command -ComputerName $hostName -Port $winrmPort -Credential $cred -SessionOption $soptions -UseSSL -ScriptBlock {Start-ADSyncSyncCycle -PolicyType delta}
