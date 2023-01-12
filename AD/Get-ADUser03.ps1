3fd52f8f85b07b764d837b32fa3e0ed0a950de0b
Karol <93782725+petitess@users.noreply.github.com>
2023-01-12 10:41:59 +0100

    Account Expiration Date

--- AD/Get-ADUser03.ps1
+++ AD/Get-ADUser03.ps1
@@ -1,3 +1,5 @@
 $username = "ven.sus.and"
 
 $user = Get-ADUser $username -Properties AccountExpirationDate |Select SAMAccountName,@{Name='AccountExpiration'; Expression={if($null -eq $_.AccountExpirationDate){'Never Expires'}else{$_.AccountExpirationDate}}}
+
+Get-ADUser -Filter {name -like $username -or samaccountname -like $username} | select SamAccountName, Enabled
