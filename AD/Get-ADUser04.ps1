40453705870f6ac0210069837c110b457defed4d
Karol <93782725+petitess@users.noreply.github.com>
2023-01-14 18:01:41 +0100

    Account Expiration Date Passed

--- AD/Get-ADUser04.ps1
+++ AD/Get-ADUser04.ps1
@@ -1,1 +1,3 @@
+$UsersOrganizationalUnit = 'OU=Vendor,OU=Accounts,OU=COMP,DC=ad,DC=comp,DC=se'
+
 Get-ADUser -Filter * -SearchBase $UsersOrganizationalUnit -Properties AccountExpirationDate, accountExpires | Where-Object {$_.enabled -eq $true -and $_.AccountExpirationDate -lt  (Get-Date) -and $_.AccountExpirationDate -notlike $null} | Select SamAccountName, AccountExpirationDate
