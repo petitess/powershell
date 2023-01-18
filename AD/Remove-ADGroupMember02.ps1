e2c32336cc552137c17fcee1c12e2ac92f582de0
Karol <93782725+petitess@users.noreply.github.com>
2023-01-18 14:10:01 +0100

    Remove Groups from users

---
+++ AD/Remove-ADGroupMember02.ps1
@@ -0,0 +1,17 @@
+$UsersOrganizationalUnit = 'OU=Vendor,OU=Accounts,OU=COMP,DC=ad,DC=comp,DC=se'
+$GroupssOrganizationalUnit = 'OU=Groups,OU=COMP,DC=ad,DC=comp,DC=se'
+
+$Users = Get-ADUser -Filter * -SearchBase $UsersOrganizationalUnit -Properties AccountExpirationDate, accountExpires | `
+Where-Object {$_.AccountExpirationDate -lt (Get-Date) -and $_.enabled -eq $true -and $_.AccountExpirationDate -notlike $null}
+
+$Groups = Get-ADGroup -Filter * -SearchBase $GroupssOrganizationalUnit
+
+foreach ($Group in $Groups) {
+Remove-ADGroupMember -Identity $Group -Members $Users -Confirm:$false
+}
+
+foreach ($User in $Users) {
+Disable-ADAccount -Identity $User.SamAccountName
+}
+
+$error >  C:\B3\Automation-Disable_Vendor_Users.ps1_errors.txt
