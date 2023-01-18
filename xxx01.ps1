7982491da4924040935386eaa0630617873118ad
Karol <93782725+petitess@users.noreply.github.com>
2023-01-18 13:45:13 +0100

    Update xxx01.ps1

--- xxx01.ps1
+++ xxx01.ps1
@@ -1,1 +1,9 @@
 Set-ADUser jdoe -Replace @{thumbnailPhoto=([byte[]](Get-Content "C:\photos\jdoe_photo.jpg" -Encoding byte))}
+
+$dc = "dc-name.domain.local:389"
+$dn = Get-ADUser joe | Select-Object -ExpandProperty distinguishedName
+[byte[]]$jpg = Get-Content C:\photos\jdoe_photo.jpg -encoding byte
+$user = [adsi]"LDAP://$dcname/$dn"
+$user.Properties["jpegPhoto"].Clear()
+$null = $user.Properties["jpegPhoto"].Add($jpg)
+$user.CommitChanges()
