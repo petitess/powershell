0b68d8079bd371d350308eb8a74d9de2cce3a4d0
Karol <93782725+petitess@users.noreply.github.com>
2023-02-12 08:34:27 +0100

    Replace value

---
+++ Get-Content01.ps1
@@ -0,0 +1,7 @@
+﻿Start-Process C:\Users\$env:username\AppData\Roaming\reolink\userSettings
+
+Get-Content -path D:\userSettings
+
+(Get-Content -path C:\Users\$env:username\AppData\Roaming\reolink\userSettings -Raw) `
+ -replace '{"key":"hardware-acceleration","type":"boolean","value":true,"_id":"','{"key":"hardware-acceleration","type":"boolean","value":false,"_id":"'`
+ | Set-Content -Path C:\Users\$env:username\AppData\Roaming\reolink\userSettings
