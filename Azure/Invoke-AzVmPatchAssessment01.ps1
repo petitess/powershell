31728b79c004f37268c64d1faddaaa8a9f7df902
Karol <93782725+petitess@users.noreply.github.com>
2023-02-15 14:37:16 +0100

    Update management center

--- Azure/Invoke-AzVmPatchAssessment01.ps1
+++ Azure/Invoke-AzVmPatchAssessment01.ps1
@@ -16,3 +16,18 @@ Invoke-AzVMInstallPatch `
 -RebootSetting IfRequired `
 -MaximumDuration PT4H `
 -ClassificationToIncludeForLinux Critical, Security
+
+$TagKey = "UpdateManagement"
+$TagValues = "Critical_Monthly_GroupA"
+
+$VMs = Get-AzVM | Where-Object {$_.Tags.Keys -eq $TagKey -and $_.Tags.Values -eq $TagValues}
+$VMs
+ForEach ($VM in $VMs) {
+    Invoke-AzVMInstallPatch  `
+    -ResourceGroupName rg-vmmgmtprod01 `
+    -VMName vmmgmtprod01 `
+    -Windows `
+    -RebootSetting IfRequired `
+    -MaximumDuration PT4H `
+    -ClassificationToIncludeForWindows Critical, Security
+}
