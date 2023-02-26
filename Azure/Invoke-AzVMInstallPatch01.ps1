fc6a4ee3ea768c916a3a99ef579201613314ffbc
Karol <93782725+petitess@users.noreply.github.com>
2023-02-26 09:56:53 +0100

    Update and rename Invoke-AzVmPatchAssessment01.ps1 to Invoke-AzVMInstallPatch01.ps1

--- Azure/Invoke-AzVMInstallPatch01.ps1
+++ Azure/Invoke-AzVMInstallPatch01.ps1
@@ -1,14 +1,3 @@
-Invoke-AzVmPatchAssessment -ResourceGroupName rg-vmmgmtprod01 -VMName vmmgmtprod01
-
-Invoke-AzVMInstallPatch  `
--ResourceGroupName rg-vmmgmtprod01 `
--VMName vmmgmtprod01 `
--Windows `
--RebootSetting IfRequired `
--MaximumDuration PT4H `
--ClassificationToIncludeForWindows Definition, Critical, Security
-
-
 Invoke-AzVMInstallPatch `
 -ResourceGroupName rg-vmmonprod01 `
 -VMName vmmonprod01 `
