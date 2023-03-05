c222be6999c8971e99208591a8c490310aa2027c
Karol <93782725+petitess@users.noreply.github.com>
2023-03-05 12:09:37 +0100

    Remove old snapshots

--- Azure/Remove-AzSnapshot01.ps1
+++ Azure/Remove-AzSnapshot01.ps1
@@ -1,6 +1,6 @@
 $snapshotResourceGroupName = "rg-snap"
 
-$OldSnapshots = Get-AzSnapshot -ResourceGroupName $snapshotResourceGroupName | Where-Object {$_.TimeCreated -lt (Get-Date).AddDays(-7) -and $_.Name -match "vmdc"}
+$OldSnapshots = Get-AzSnapshot -ResourceGroupName $snapshotResourceGroupName | Where-Object {$_.TimeCreated -lt (Get-Date).AddDays(-5) -and $_.Name -match "vmdc"}
 $OldSnapshots.Count
 foreach ($OldSnapshot in $OldSnapshots) {
     Remove-AzSnapshot -ResourceGroupName $snapshotResourceGroupName -SnapshotName $OldSnapshot.Name -Force -Confirm:$false
