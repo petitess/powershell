de16aada4c240f67c54cf8723823c69e1cc665dc
Karol <93782725+petitess@users.noreply.github.com>
2023-03-28 17:57:02 +0200

    Connects to SFTP server and downloads files.

---
+++ Get-SFTPItem01.ps1
@@ -0,0 +1,45 @@
+﻿#Install module
+#install-module posh-ssh -Confirm:$false
+#Get-Command *sftp*
+
+#This script connects to a SFTP server
+#Downloads files from SFTP to a folder with today's date
+#Compares file size with files in the main folder
+#If there is a difference copies new files to main folder
+#Removes folders older than 3 days
+
+$ErrorActionPreference = "Stop"
+$password = ConvertTo-SecureString "xxxxW3uxxx" -AsPlainText -Force
+$creds = New-Object System.Management.Automation.PSCredential ("username", $password)
+$session = New-SFTPSession -Computername sftp.site.net -Port 2222 -Credential $creds -Verbose
+$source = "/"
+$items = Get-SFTPChildItem -Recursive $session -Path $source
+$localDestination = "D:\AVD"
+$date = Get-Date -Format 'yyyy-MM-dd-HH'
+
+if (Test-Path -Path "$localDestination\Filer_$date") {
+Write-Output "Folder Filer_$date exists"
+}else{
+New-Item -Path "$localDestination\Filer_$date" -ItemType Directory -Force
+}
+
+foreach ($item in $items) {
+Get-SFTPItem -SessionId $session.SessionID -Path $item.FullName -Destination "$localDestination\Filer_$date" -Force
+}
+
+$latestFolder = Get-ChildItem -Path $localDestination -Directory | select -last 1
+$lastestFiles = Get-ChildItem -Path $localDestination\$latestFolder
+
+foreach ($file in $lastestFiles) {
+
+if($file.Length -eq (Get-ChildItem -Path $localDestination | where {$_.Name -eq $file}).Length) {
+Write-Output "The same size: $file"}
+
+else{
+
+Copy-Item -Path $localDestination\$latestFolder\$file -Destination $localDestination -Force
+}
+}
+
+$oldFolders = Get-ChildItem $localDestination | where {$_.Name -match "Filer" -and $_.CreationTime -lt (Get-Date).AddDays(-3)}
+$oldFolders | Remove-Item -Force -Confirm:$false
