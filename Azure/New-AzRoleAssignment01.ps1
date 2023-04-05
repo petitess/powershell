906275cb6ccd6970d2ee31a1e242d5a6cb598c40
Karol <93782725+petitess@users.noreply.github.com>
2023-04-05 10:51:11 +0200

    App registration tenant permission

--- Azure/New-AzRoleAssignment01.ps1
+++ Azure/New-AzRoleAssignment01.ps1
@@ -3,3 +3,7 @@ Connect-AzAccount
 $app = Get-AzADServicePrincipal -DisplayName "xxx-Infrastruktur-sp-governance-01"
 
 New-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Owner' -ObjectId $app.Id
+
+Get-AzRoleAssignment -ObjectId $app.Id -Scope "/"
+
+Remove-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Owner' -ObjectId $app.Id
