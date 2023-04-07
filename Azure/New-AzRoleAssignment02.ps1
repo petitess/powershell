2936b60d2fd001014c911f35ede9e5f5a5bc89e7
Karol <93782725+petitess@users.noreply.github.com>
2023-04-07 02:28:48 +0200

    User tenant permission

--- Azure/New-AzRoleAssignment02.ps1
+++ Azure/New-AzRoleAssignment02.ps1
@@ -3,3 +3,8 @@ Connect-AzAccount
 $user = Get-AzADUser -UserPrincipalName ""
 
 New-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Owner' -ObjectId $user.Id
+
+Get-AzRoleAssignment -ObjectId $user.Id -Scope "/"
+
+Remove-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Owner' -ObjectId $user.Id
+
