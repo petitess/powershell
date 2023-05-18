4b5bcdc2158b90f09332a18afa8f13a76e5397ab
Karol <93782725+petitess@users.noreply.github.com>
2023-05-18 09:36:40 +0200

    Terms

--- Azure/Get-AzMarketplaceTerms02.ps1
+++ Azure/Get-AzMarketplaceTerms02.ps1
@@ -1,2 +1,1 @@
-$terms = Get-AzMarketplaceTerms -Publisher citrix -Product netscalervpx-131 -Name netscalerbyol
-Set-AzMarketplaceTerms -Publisher citrix -Product netscalervpx-131 -Name netscalerbyol -Terms $terms -Accept
+Set-AzMarketplaceTerms -Publisher "center-for-internet-security-inc" -Product "cis-windows-server-2019-v1-0-0-l2" -Name "cis-ws2019-l2" -Accept
