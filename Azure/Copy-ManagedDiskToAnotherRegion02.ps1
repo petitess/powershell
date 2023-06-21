0395842d0c1ce2bb28e10fa0beb4b260ac594807
Karol <93782725+petitess@users.noreply.github.com>
2023-06-21 13:51:21 +0200

    Copy Azure Managed disks between regions

---
+++ Azure/Copy-ManagedDiskToAnotherRegion02.ps1
@@ -0,0 +1,23 @@
+# SOURCE
+$SnapshotResourceGroup = "rg-backup-prod-01"
+$SnapshotName = "snap-vmadcprod02-20230620-2200"
+
+# DESTINATION
+$StorageAccount = "staaatest01"
+$StorageAccountBlob = "vmadcprod01-02-vhd"
+$storageaccountResourceGroup = "rg-aaa-test"
+$vhdname = "vmadcprod02-20230620"
+
+
+#SA_KEY
+$StorageAccountKey = (Get-AzStorageAccountKey -Name $StorageAccount -ResourceGroupName $StorageAccountResourceGroup).value[0]
+$snapshot = Get-AzSnapshot -ResourceGroupName $SnapshotResourceGroup -SnapshotName $SnapshotName
+
+#GRANTING ACCESS
+$snapshotaccess = Grant-AzSnapshotAccess -ResourceGroupName $SnapshotResourceGroup -SnapshotName $SnapshotName -DurationInSecond 3600 -Access Read -ErrorAction stop 
+   
+$DestStorageContext = New-AzStorageContext –StorageAccountName $storageaccount -StorageAccountKey $StorageAccountKey -ErrorAction stop
+
+Write-Output "START COPY"
+Start-AzStorageBlobCopy -AbsoluteUri $snapshotaccess.AccessSAS -DestContainer $StorageAccountBlob -DestContext $DestStorageContext -DestBlob "$($vhdname).vhd" -Force -ErrorAction stop
+Write-Output "END COPY"
