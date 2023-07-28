002d0c8752974905ea66685ff0c9029f0c6ef05d
Karol <93782725+petitess@users.noreply.github.com>
2023-07-28 15:39:53 +0200

    Update Get-MgUser01.ps1

--- MicrosoftGraph/Get-MgUser01.ps1
+++ MicrosoftGraph/Get-MgUser01.ps1
@@ -5,3 +5,7 @@ Get-MgUser -Filter "DisplayName eq 'Karol'"
 Get-MgUser -Filter "startsWith(DisplayName, 'A')"
 
 Get-MgUser -Filter 'accountEnabled eq true' -All
+
+Get-MgUser -Filter 'accountEnabled eq true' -Search 'DisplayName:van' -ConsistencyLevel eventual
+
+Get-MgUser -UserId adelev@lazydev.onmicrosoft.com -ExpandProperty manager | Select @{Name = 'Manager'; Expression = {$_.Manager.AdditionalProperties.displayName}}
