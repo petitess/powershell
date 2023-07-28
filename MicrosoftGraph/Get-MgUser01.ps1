da18ac11f2f6f42067e5adcc1cc9b7604edbdbda
Karol <93782725+petitess@users.noreply.github.com>
2023-07-28 15:45:40 +0200

    Update Get-MgUser01.ps1

--- MicrosoftGraph/Get-MgUser01.ps1
+++ MicrosoftGraph/Get-MgUser01.ps1
@@ -9,3 +9,17 @@ Get-MgUser -Filter 'accountEnabled eq true' -All
 Get-MgUser -Filter 'accountEnabled eq true' -Search 'DisplayName:van' -ConsistencyLevel eventual
 
 Get-MgUser -UserId adelev@lazydev.onmicrosoft.com -ExpandProperty manager | Select @{Name = 'Manager'; Expression = {$_.Manager.AdditionalProperties.displayName}}
+
+$props = @(
+    # Basic metadata
+    'Id','DisplayName','Mail','UserPrincipalName','Department','JobTitle'
+    # Account Status
+    'AccountEnabled',
+    # Password last set
+    'LastPasswordChangeDateTime'
+    # Last logon
+    #'SignInActivity'
+    # Assigned Licenses
+    #'AssignedLicenses'
+)
+Get-MgUser -All -Property $props | Select-Object $props
