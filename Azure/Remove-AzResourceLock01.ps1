8e95895da0a87a2f39d91b4187a782684268fee3
Karol <93782725+petitess@users.noreply.github.com>
2023-08-02 12:44:10 +0200

    Remove Resource Lock

--- Azure/Remove-AzResourceLock01.ps1
+++ Azure/Remove-AzResourceLock01.ps1
@@ -1,4 +1,4 @@
-foreach ($lock in Get-AzResourceLock) {
+foreach ($lock in Get-AzResourceLock | Where-Object { $_.ResourceGroupName -like "rg-vm*" }) {
 
     Remove-AzResourceLock -LockId ($lock).LockId -Force
 }
