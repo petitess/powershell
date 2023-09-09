c40731e1f67b9b2455018af3b66137913a3408b5
Karol <93782725+petitess@users.noreply.github.com>
2023-09-09 13:39:20 +0200

    Create Subscriptions and Devops Service Principals(Az CLI)

--- Azure/New-AzSubscriptionAlias02.ps1
+++ Azure/New-AzSubscriptionAlias02.ps1
@@ -1,5 +1,5 @@
 
-$billingAccounts = 'xxxxxx-3b42-53ba-e814-421e30e28525:1346c60a-08c0-4567-a607-xxxxxxxxxxx'
+$billingAccounts = 'xxxxx-3b42-53ba-e814-421e30e28525:1346c60a-08c0-4567-a607-xxxxxxxxxx'
 $billingProfile = 'xxxx-Y3PM-BG7-xxx'
 $invoiceSections = 'xxxx-TWIN-PJA-xxx'
 $billingScope = "/providers/Microsoft.Billing/billingAccounts/$billingAccounts/billingProfiles/$billingProfile/invoiceSections/$invoiceSections"
@@ -10,33 +10,28 @@ $names = @(
 )
 
 $rbac = "Owner"
-$devopsOrg = "https://dev.azure.com/karolsek0491"
+$devopsOrg = "https://dev.azure.com/xxxx0491"
 $devopsPrj = "AzureIAC"
 $tenantID = "xxxx-543e-47c1-a8e6-xxxx"
 $kv = "kvsystem"
 
 foreach ($sub in $names) {
-    
-    #$newSub = New-AzSubscriptionAlias -AliasName $sub -BillingScope $billingScope -DisplayName $sub -Workload 'Production'
-    $newSub = az account alias create --name $sub --billing-scope $billingScope --display-name $sub --workload 'Production'
+    #Create subscriptions
+    az account alias create --name $sub --billing-scope $billingScope --display-name $sub --workload 'Production' 
+    $newSub = az account alias show --name $sub --query "properties.subscriptionId" -o tsv
 
-    #New-AzManagementGroupSubscription -GroupId "mg-sek-prod-01" -SubscriptionId $newSub.SubscriptionId
-    az account management-group subscription add --name "mg-sek-prod-01" --subscription $newSub.SubscriptionId
+    #Add subscriptions to management group
+    az account management-group subscription add --name "mg-sek-prod-01" --subscription $newSub
 
     $spiname = "sp-$sub"
-    $subsID = $newSub.SubscriptionId
 
     $spipasswd = az ad sp create-for-rbac -n $spiname --query "password" -o tsv
 
     # Query the Application ID of the Service Principal and Store it in a variable:-
     $spiID = az ad sp list --display-name $spiname --query [].appId -o tsv
     
-    # Store the Service Principal Application ID and Secret in Key Vault:-
-    az keyvault secret set --name $spiname-id --vault-name $kv --value $spiID
-    az keyvault secret set --name $spiname-passwd --vault-name $kv --value $spipasswd
-    
     # Assign the Service Principal, "Contributor" RBAC on Subscription Level:-
-    az role assignment create --assignee "$spiID" --role "$rbac" --scope "/subscriptions/$subsID"
+    az role assignment create --assignee "$spiID" --role "$rbac" --scope "/subscriptions/$newSub"
     
     #Set Service Principal Secret as an Environment Variable for creating Azure DevOps Service Connection:-
     $env:AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY = $spipasswd
@@ -48,7 +43,7 @@ foreach ($sub in $names) {
     az devops configure --defaults organization=$devopsOrg project=$devopsPrj
     
     # Create DevOps Service Connection:-
-    az devops service-endpoint azurerm create --azure-rm-service-principal-id $spiID --azure-rm-subscription-id $subsID --azure-rm-subscription-name $sub --azure-rm-tenant-id $tenantID --name $spiname --org $devopsOrg --project $devopsPrj
+    az devops service-endpoint azurerm create --azure-rm-service-principal-id $spiID --azure-rm-subscription-id $newSub --azure-rm-subscription-name $sub --azure-rm-tenant-id $tenantID --name $spiname --org $devopsOrg --project $devopsPrj
     
     # Grant Access to all Pipelines to the Newly Created DevOps Service Connection:-
     $srvEndpointID = az devops service-endpoint list --query "[?name=='$spiname'].id" -o tsv
