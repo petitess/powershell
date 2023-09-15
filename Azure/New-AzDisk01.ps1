d0fe2b551c12a6e9c6ffe2f496e81df5013876a2
Karol <93782725+petitess@users.noreply.github.com>
2023-09-15 15:36:22 +0200

    Create managed disk from VHD

--- Azure/New-AzDisk01.ps1
+++ Azure/New-AzDisk01.ps1
@@ -5,10 +5,10 @@ $vhdUri = "https://stxrecoveryprod02.blob.core.windows.net/$stContainer/"
 
 $st = Get-AzStorageAccount -Name $stName  -ResourceGroupName $stRgName
 $stKey = (Get-AzStorageAccountKey -ResourceGroupName $stRgName -Name $stName).Value[0]
-$storage_account = New-AzStorageContext -StorageAccountName $stName  -StorageAccountKey $stKey  ##"YzaAG/cBVeOQffdTfLlYNWqmW+Mx2pFSLR9UfRgrnu5CsXOOnB7mjSWtfc/OxvRRpo7XmkitDr4p+AStgW3Smw=="
+$storage_account = New-AzStorageContext -StorageAccountName $stName  -StorageAccountKey $stKey
 
 $containers = Get-AzStorageContainer -Context $storage_account -Name $stContainer
-$blobs = Get-AzStorageBlob -Container $containers.Name -Context $storage_account | Where-Object {$_.Name -match "vmmgmt"}
+$blobs = Get-AzStorageBlob -Container $containers.Name -Context $storage_account | Where-Object {$_.Name -match "vmmgmt" -and $_.BlobProperties.BlobCopyStatus -eq "Success"}
 
-$config = New-AzDiskConfig -Location "westeurope" -SkuName Premium_LRS -DiskSizeGB ($blobs[1] | ForEach-Object {[math]::truncate($_.Length / 1GB)}) -OsType Windows -HyperVGeneration V1 -Architecture X64 -CreateOption Import -SourceUri ($vhdUri+$blobs[1].Name) -StorageAccountId $st.Id
+$config = New-AzDiskConfig -Location "westeurope" -SkuName Premium_LRS -DiskSizeGB ($blobs[0] | ForEach-Object {[math]::truncate($_.Length / 1GB)}) -OsType Windows -HyperVGeneration V1 -Architecture X64 -CreateOption Import -SourceUri ($vhdUri+$blobs[0].Name) -StorageAccountId $st.Id
 New-AzDisk -ResourceGroupName "rg-vmmgmtprod01" -DiskName 'vmmgmt-disk' -Disk $config
