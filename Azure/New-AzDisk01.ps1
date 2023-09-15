e5afed1f6cad915f3289efc767ff251f7e23795f
Karol <93782725+petitess@users.noreply.github.com>
2023-09-15 15:30:57 +0200

    Create managed disk from VHD

---
+++ Azure/New-AzDisk01.ps1
@@ -0,0 +1,14 @@
+$stName = "stxrecoveryprod02" 
+$stRgName = "rg-xrecovery-prod-01"
+$stContainer = "care-vhd-we"
+$vhdUri = "https://stxrecoveryprod02.blob.core.windows.net/$stContainer/"
+
+$st = Get-AzStorageAccount -Name $stName  -ResourceGroupName $stRgName
+$stKey = (Get-AzStorageAccountKey -ResourceGroupName $stRgName -Name $stName).Value[0]
+$storage_account = New-AzStorageContext -StorageAccountName $stName  -StorageAccountKey $stKey  ##"YzaAG/cBVeOQffdTfLlYNWqmW+Mx2pFSLR9UfRgrnu5CsXOOnB7mjSWtfc/OxvRRpo7XmkitDr4p+AStgW3Smw=="
+
+$containers = Get-AzStorageContainer -Context $storage_account -Name $stContainer
+$blobs = Get-AzStorageBlob -Container $containers.Name -Context $storage_account | Where-Object {$_.Name -match "vmmgmt"}
+
+$config = New-AzDiskConfig -Location "westeurope" -SkuName Premium_LRS -DiskSizeGB ($blobs[1] | ForEach-Object {[math]::truncate($_.Length / 1GB)}) -OsType Windows -HyperVGeneration V1 -Architecture X64 -CreateOption Import -SourceUri ($vhdUri+$blobs[1].Name) -StorageAccountId $st.Id
+New-AzDisk -ResourceGroupName "rg-vmmgmtprod01" -DiskName 'vmmgmt-disk' -Disk $config
