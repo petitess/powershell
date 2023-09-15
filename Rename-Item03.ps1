66266d06162fd407b63b9a97255802ac83208b76
Karol <93782725+petitess@users.noreply.github.com>
2023-09-15 12:46:32 +0200

    Substring function

---
+++ Rename-Item03.ps1
@@ -0,0 +1,9 @@
+#Remove the last character from a string
+$Name = 'vmadcprod03_OsDisk_1_a491e9ab84ec436fa00d4c500f11d24f-20230915'
+
+$Name.Substring(0,$ComputerName.Length-8)
+
+"$((($Name).Replace('snap-', '')).Substring(0,($Name).Length-14)).vhd"
+
+
+
