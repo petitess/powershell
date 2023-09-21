ae6c2e72e71b385b4b89b7ac18e1b193377691e4
Karol <93782725+petitess@users.noreply.github.com>
2023-09-21 18:00:08 +0200

    Get Logged on Users

--- GetLoggedOnUsers.ps1
+++ GetLoggedOnUsers.ps1
@@ -5,3 +5,7 @@
 ((Get-WMIObject -ClassName Win32_ComputerSystem).Username).Split('\')[1]
 
 [System.Environment]::UserName
+
+query user
+
+((query user) -split "\n" -replace '\s\s+', ';' | convertfrom-csv -Delimiter ';').STATE
