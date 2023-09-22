0bfc4cdcfe89b71dcc1cd4e0cda6f25dcdb0a9e3
Karol <93782725+petitess@users.noreply.github.com>
2023-09-22 03:40:21 +0200

    Get logged on and active users v2

---
+++ Azure/Invoke-AzVMRunCommand02.ps1
@@ -0,0 +1,21 @@
+$TagKey = "AutoShutdown"
+$TagValues = "GroupC"
+
+$VMs = Get-AzVM | Where-Object { $_.Tags.Keys -eq $TagKey -and $_.Tags.Values -eq $TagValues }
+
+ForEach ($VM in $VMs) {
+
+    $scriptCode = "if (((query user) -split '\n' -replace '\s\s+', ';' | convertfrom-csv -Delimiter ';').STATE -contains 'Active') {Write-Output 'ActiveUsers'}else {Write-Output 'ShutdownSystem'}"
+    #$scriptCode = {$X = ((query user) -split "\n" -replace '\s\s+', ';' | convertfrom-csv -Delimiter ';').STATE;if ($X -contains "Active") {Write-Output "ActiveUsers"}else {Write-Output "ShutdownSystem"}}
+
+    $I = Invoke-AzVMRunCommand -ResourceGroupName $VM.ResourceGroupName -Name $VM.Name -CommandId 'RunPowerShellScript' -ScriptString $scriptCode
+    $output = $I.Value[0].Message
+
+    if ($output -eq "ShutdownSystem") {
+        #Stop-AzVM -ResourceGroupName $VM.ResourceGroupName -Name $VM.Name -Force
+        Write-Output "$($VM.Name) No active users on the system. Shutting down."
+    }
+    else {
+        Write-Output "$($VM.Name) Active users on the system"
+    }
+}
