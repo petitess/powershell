e00c9b264a942e7578b237c02069093b6fe4b9b8
Karol <93782725+petitess@users.noreply.github.com>
2023-10-01 14:08:26 +0200

    Get Alert rules from Action Groups

--- Azure/Get-AzScheduledQueryRule01.ps1
+++ Azure/Get-AzScheduledQueryRule01.ps1
@@ -4,5 +4,5 @@ $sub | ForEach-Object {
     Set-AzContext -Subscription $_.Name
     $agName = "AG-Critical-Guld-LogAnalytics"
     $ag = Get-AzActionGroup | Where-Object {$_.Name -eq $agName}
-    Get-AzScheduledQueryRule | Where-Object { $_.ActionGroup -match $ag.id } | Select-Object Name, $agName | Out-File ($agName + ".txt") -Append
+    Get-AzScheduledQueryRule | Where-Object { $_.ActionGroup -eq $ag.id } | Select-Object Name, {$ag.Name} | Out-File ($agName + ".txt") -Append
 } 
