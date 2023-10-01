3a638c8226e595fba2f0ef02935abdd7b4ff72fc
Karol <93782725+petitess@users.noreply.github.com>
2023-10-01 15:35:57 +0200

    Custom properties

--- Select-Object01.ps1
+++ Select-Object01.ps1
@@ -1,3 +1,17 @@
 Get-ComputerInfo | Select-Object CsDNSHostName, @{Name="Win version"; Expression = {$_.WindowsProductName +' '+ $_.WindowsVersion}}
 
 Get-Process | Select-Object -Property ProcessName,@{Name="Start Day"; Expression = {$_.StartTime.DayOfWeek}}
+
+Get-Process | Select-Object Name, CPU, @{Name="Memory (MB)";Expression={$_.PM/1MB}} 
+
+Get-Service | Select-Object DisplayName, Status, @{Name='Comments'; Expression={if ($_.Status -eq 'Running') {'All good!'} else {'Needs attention.'}}}
+
+Get-ComputerInfo | Select-Object `
+CsDNSHostName, `
+@{Name="WindowsProductId"; Expression = {($_.WindowsProductId).Substring(0,10)}}
+
+Get-ComputerInfo | Select-Object `
+CsDNSHostName, `
+@{label="Win version"; Expression = {$_.WindowsProductName +' '+ $_.WindowsVersion}}, `
+BiosName, `
+@{label="WindowsProductId"; Expression = {($_.WindowsProductId).Substring(0,10)}}
