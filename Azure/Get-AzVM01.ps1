66ea4ee71368159c5aebc77c8af3e43df48ed98a
Karol <93782725+petitess@users.noreply.github.com>
2023-10-13 17:20:13 +0200

    Get VMs

--- Azure/Get-AzVM01.ps1
+++ Azure/Get-AzVM01.ps1
@@ -1,1 +1,5 @@
 Get-AzVM -Status | Where-Object { $_.PowerState -match "VM running" -and $_.Name -notmatch "vmvda"} 
+
+$TagKey = "AutoShutdown"
+$VMs = Get-AzVM -Status | Where-Object { $_.Tags.Keys -eq $TagKey -and $_.Name -match "vmvdaprod" -and $_.PowerState -eq "VM running" }
+$VMs | Select-Object -Last 1
