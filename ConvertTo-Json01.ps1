9e42e846ec8e2a98447bf84f7fc799f35a68ea57
Karol <93782725+petitess@users.noreply.github.com>
2023-10-18 07:15:11 +0200

    ConvertTo-Json

--- ConvertTo-Json01.ps1
+++ ConvertTo-Json01.ps1
@@ -14,3 +14,29 @@ $json = $obj | ConvertTo-Json
 
 # Save JSON to file
 $json | Set-Content -Path C:\alkane\example.json
+#############
+#############
+$json = @"
+{
+"Stuffs": 
+    [
+        {
+            "Name": "Darts",
+            "Type": "Fun Stuff"
+        },
+
+        {
+            "Name": "Clean Toilet",
+            "Type": "Boring Stuff"
+        }
+    ]
+}
+"@
+
+$x = $json | ConvertFrom-Json
+
+$x.Stuffs[0] # access to Darts
+$x.Stuffs[1] # access to Clean Toilet
+$darts = $x.Stuffs | where { $_.Name -eq "Darts" } #Darts
+#############
+#############
