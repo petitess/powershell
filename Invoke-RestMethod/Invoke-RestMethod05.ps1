b86c7766c4ce1310d0c9486f0a4ffc1b5d118803
Karol <93782725+petitess@users.noreply.github.com>
2023-10-25 14:09:02 +0200

    Update DevOps Service Connection to Workload Identity Federation

--- Invoke-RestMethod/Invoke-RestMethod05.ps1
+++ Invoke-RestMethod/Invoke-RestMethod05.ps1
@@ -1,11 +1,11 @@
 $devopsOrgName = "xxx"
 $spiname = "sp-infra-dev-01"
+$serviceEndpointId = az devops service-endpoint list --query "[?name=='$spiname'].id" -o tsv
 $projectName = "Infrastruktur"
 $projectId = az devops project show --project $projectName --query "id" --output tsv
 $token = az account get-access-token --query accessToken --output tsv
 $body = ConvertTo-Json -Depth 10 @{
     type = "azurerm"
-    id = $id
     authorization = @{
         scheme = "WorkloadIdentityFederation"
         #scheme = "ServicePrincipal"
@@ -22,4 +22,4 @@ $body = ConvertTo-Json -Depth 10 @{
     )
 }
 
-Invoke-RestMethod  -Method PUT -Uri "https://dev.azure.com/$devopsOrgName/$devopsProjectName/_apis/serviceendpoint/endpoints/${id}?operation=ConvertAuthenticationScheme&api-version=7.2-preview.4" -Headers @{ Authorization = "Bearer $token"; "Content-Type" = "application/json"} -Body $body
+Invoke-RestMethod  -Method PUT -Uri "https://dev.azure.com/$devopsOrgName/$devopsProjectName/_apis/serviceendpoint/endpoints/${serviceEndpointId}?operation=ConvertAuthenticationScheme&api-version=7.2-preview.4" -Headers @{ Authorization = "Bearer $token"; "Content-Type" = "application/json"} -Body $body
