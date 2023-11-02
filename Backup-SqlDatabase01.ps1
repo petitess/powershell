6328f109d6cf239f2afbc6f43e943cbff3cd0402
Karol <93782725+petitess@users.noreply.github.com>
2023-11-02 15:43:22 +0100

    Backup SQL Database to storage account

---
+++ Backup-SqlDatabase01.ps1
@@ -0,0 +1,10 @@
+$database = (Get-SqlDatabase -ServerInstance "vmsqlprod01").Name
+
+$database | ForEach-Object {
+    $date = Get-Date -Format "yyyy_MM_dd_HH_mm"
+    $container = 'https://stbackupXXXXprod01.blob.core.windows.net/vmsqlprod01-full-backup'
+    $fileName = $_ + "_" + $date+".bak"
+    $server = 'vmsqlprod01'
+    $backupFile = $container + '/' + $fileName
+    Backup-SqlDatabase -ServerInstance $server -Database $_ -BackupFile $backupFile
+}
