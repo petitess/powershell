a4abe7937b136c6299ae7d80403033901e578adf
Karol <93782725+petitess@users.noreply.github.com>
2023-11-03 08:55:04 +0100

    Backup SQL Database to Storage Account - Transaction Log

--- Backup-SqlDatabase02.ps1
+++ Backup-SqlDatabase02.ps1
@@ -3,7 +3,7 @@ $database = (Get-SqlDatabase -ServerInstance $server | Where-Object {$_.Recovery
 $database | ForEach-Object {
     $date = Get-Date -Format "yyyy_MM_dd_HH_mm"
     $container = 'https://stbackupXXXprod02.blob.core.windows.net/vmsqlprod01-transaction-log'
-    $fileName = $container + '/' + $_ + "_" + $date+".bak"
+    $fileName = $container + '/' + $_ + "_" + $date +".bak"
     Backup-SqlDatabase -ServerInstance $server -Database $_ `
         -CompressionOption On -MaxTransferSize 4194304 -BlockSize 65536 `
         -BackupFile $fileName
