ffd4afcf358bb6a23abadd78b6988a434e75da57
Karol <93782725+petitess@users.noreply.github.com>
2023-11-20 09:38:57 +0100

    Root, client, VPN

--- New-SelfSignedCertificate02.ps1
+++ New-SelfSignedCertificate02.ps1
@@ -20,7 +20,7 @@ $cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
     -HashAlgorithm sha256 `
     -KeyLength 2048 `
     -CertStoreLocation "Cert:\CurrentUser\My" `
-    -KeyUsageProperty Sign -KeyUsage CertSign
+    -KeyUsageProperty Sign -KeyUsage CertSign -NotAfter (Get-Date).AddMonths(24)
 
 ##Generate a client certificate
 
@@ -34,7 +34,7 @@ New-SelfSignedCertificate `
     -KeyLength 2048 `
     -CertStoreLocation "Cert:\CurrentUser\My" `
     -Signer $cert `
-    -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
+    -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2") -NotAfter (Get-Date).AddMonths(24)
 
 ##Export Certificates
 
