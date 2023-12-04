2cf8c84a3c72b91ba0f4c0593203c59fa14c5fc5
Karol <93782725+petitess@users.noreply.github.com>
2023-12-04 06:13:14 +0100

    create/update resource group

--- Invoke-RestMethod/Invoke-RestMethod01.ps1
+++ Invoke-RestMethod/Invoke-RestMethod01.ps1
@@ -1,3 +1,4 @@
+###CREATE
 $subscriptionId = (Get-AzSubscription -SubscriptionName "Name").Id
 $RessourceGroupName = "rg-restapi01"
 $Location = "northeurope"
@@ -16,5 +17,27 @@ $bodyNewRessourceGroup = @"
 "@
 
 Invoke-RestMethod -Method PUT -URI $URL -body $bodyNewRessourceGroup -Headers $headers
+###UPDATE
+$subscriptionId = (Get-AzSubscription -SubscriptionName "sub-default-01").Id
+$RessourceGroupName = "rg-xxx"
+$Location = "swedencentral"
+$apiversion="2022-09-01"
+$URL = "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$($RessourceGroupName)?api-version=$apiversion"
+
+$headers = @{
+    "Authorization" = "Bearer $((Get-AzAccessToken).Token)"
+    "Content-type"  = "application/json"
+  }
+
+$bodyNewRessourceGroup = @"
+    {
+        "location": "$location",
+        "tags": {
+            "SYSTEM" : "$(Get-Date)"
+        }
+    }
+"@
+
+Invoke-RestMethod -Method PATCH -URI $URL -body $bodyNewRessourceGroup -Headers $headers
 
 
