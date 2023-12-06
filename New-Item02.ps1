50a493920e25759d316743eabb730b07b4535995
Karol <93782725+petitess@users.noreply.github.com>
2023-12-06 09:13:08 +0100

    Update New-Item02.ps1

--- New-Item02.ps1
+++ New-Item02.ps1
@@ -1,8 +1,6 @@
 New-Item -ItemType SymbolicLink -Path "C:\Temp\stinfrab3careprod01\fileshare01" -Target "\\10.10.3.5\fileshare01"
 #######
-New-Item -Path "C:\Windows" -Name "0Users" -ItemType Directory -Force
-$User = "localhost\stavdprofilesdev02"
-$PWord = "xxxxxecYv4L0SHtLoU0QieUhhpbdAvBHtBfZnCnVhLA1Nmr1RuhwGqh277BqG+AStJVnLNw=="
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential($User, $PWord)
-$Cred = Get-Credential -Credential $Credential
-New-Item -ItemType SymbolicLink -Target "C:\Windows\0Users" -Path "\\stavdprofilesdev02.file.core.windows.net\profiles" -Credential $Cred 
+$secPassword = ConvertTo-SecureString "ssscYv4L0SHtLoU0QieUhhpbdAvBHtBfZnCnVhLA1Nmr1RuhwGqh277BqG+AStJVnLNw==" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ("localhost\stavdprofilesdev02", $secPassword)
+$Credential = Get-Credential -Credential $cred
+New-Item -ItemType SymbolicLink -Target "C:\Windows\0Users" -Path "\\stavdprofilesdev02.file.core.windows.net\profiles" -Credential $Credential
