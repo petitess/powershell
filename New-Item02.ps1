eea960e07e53d10c3666d695789db2a4185cafca
Karol <93782725+petitess@users.noreply.github.com>
2023-12-06 10:33:08 +0100

    Update New-Item02.ps1

--- New-Item02.ps1
+++ New-Item02.ps1
@@ -1,5 +1,5 @@
 New-Item -ItemType SymbolicLink -Path "C:\Temp\stinfrab3careprod01\fileshare01" -Target "\\10.10.3.5\fileshare01"
-#######
+#######DOESNT WORK. YOU HAVE TO PUT PASSWORD MANUALLY.
 $secPassword = ConvertTo-SecureString "ssscYv4L0SHtLoU0QieUhhpbdAvBHtBfZnCnVhLA1Nmr1RuhwGqh277BqG+AStJVnLNw==" -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ("localhost\stavdprofilesdev02", $secPassword)
 $Credential = Get-Credential -Credential $cred
