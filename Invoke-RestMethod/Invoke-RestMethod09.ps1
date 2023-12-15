8260eeb7938aec933fa6debb1701487b51d2b218
Karol <93782725+petitess@users.noreply.github.com>
2023-12-15 16:47:24 +0100

    Local server - swagger

--- Invoke-RestMethod/Invoke-RestMethod09.ps1
+++ Invoke-RestMethod/Invoke-RestMethod09.ps1
@@ -21,3 +21,43 @@ $body = @"
     }
 "@
 Invoke-RestMethod -Method POST -URI $URL -Headers $headers -body $body
+##PUT
+$Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMDIyIiwibmJmIjoxNzAyNDg3MzQ5LCJleHAiOjE3MDI2NjAxNDksImlhdCI6MTcwMjQ4NzM0OX0.-N_ppHZF0oqjJ8tfl3DvrrjanVerWOKQpO1VuvBavOk"
+$URL = "http://localhost:5000/Post/UpsertPost"
+$headers = @{
+    
+    "Authorization" = "Bearer $Token"
+    "Content-type"  = "application/json"
+}
+$body = @"
+    {
+        "postTitle": "COMPUTER",
+        "postContent": "ThinkSmart"
+    }
+"@
+Invoke-RestMethod -Method PUT -URI $URL -Headers $headers -body $body
+##PUT
+$Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMDIyIiwibmJmIjoxNzAyNDg3MzQ5LCJleHAiOjE3MDI2NjAxNDksImlhdCI6MTcwMjQ4NzM0OX0.-N_ppHZF0oqjJ8tfl3DvrrjanVerWOKQpO1VuvBavOk"
+$URL = "http://localhost:5000/Post/UpsertPost"
+$headers = @{
+    
+    "Authorization" = "Bearer $Token"
+    "Content-type"  = "application/json"
+}
+$body = @"
+    {
+        "postId" : "7",
+        "postTitle": "COMPUTER",
+        "postContent": "Lenovo ThinkSmart"
+    }
+"@
+Invoke-RestMethod -Method PUT -URI $URL -Headers $headers -body $body
+##DELETE
+$Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMDIyIiwibmJmIjoxNzAyNDg3MzQ5LCJleHAiOjE3MDI2NjAxNDksImlhdCI6MTcwMjQ4NzM0OX0.-N_ppHZF0oqjJ8tfl3DvrrjanVerWOKQpO1VuvBavOk"
+$URL = "http://localhost:5000/Post/DeletePost/9"
+$headers = @{
+    
+    "Authorization" = "Bearer $Token"
+    "Content-type"  = "application/json"
+}
+Invoke-RestMethod -Method Delete -URI $URL -Headers $headers
