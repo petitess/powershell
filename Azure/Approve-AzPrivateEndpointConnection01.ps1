24bc357a87d28fb8303cb7af8c6b8b4bab993bcd
Karol <93782725+petitess@users.noreply.github.com>
2023-12-20 14:50:39 +0100

    Approve Private Endpoint Connection - runbook

--- Azure/Approve-AzPrivateEndpointConnection01.ps1
+++ Azure/Approve-AzPrivateEndpointConnection01.ps1
@@ -1,11 +1,12 @@
 param (
     [Parameter(Mandatory)]
-    [String]$SubId
+    [string]$EXTSUBID
 )
-
 Connect-AzAccount -Identity
 
-Set-AzContext -Subscription $SubId
+Set-AzContext -Subscription "$EXTSUBID"
+Write-Output $("INFO: " + ((Get-AzContext).Name).ToUpper())
+Write-Output $("ID: " + $EXTSUBID)
 
 $StId = (Get-AzStorageAccount).id
 $AppId = (Get-AzWebApp).Id
@@ -13,11 +14,12 @@ $AppId = (Get-AzWebApp).Id
 $StId + $AppId | ForEach-Object {
     $PepId = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $_ | Where-Object { $_.PrivateLinkServiceConnectionState.Status -eq "Pending" }
     if ($null -ne $PepId) {
-    (Approve-AzPrivateEndpointConnection -ResourceId $PepId[0]).Name
-    $Status = (Get-AzPrivateEndpointConnection -ResourceId $PepId[0].Id).PrivateLinkServiceConnectionState.Status
-    $Name = (Get-AzPrivateEndpointConnection -ResourceId $PepId[0].Id).Name
-    Write-Output $($Status + ": " + $Name)
-    } else {
+        (Approve-AzPrivateEndpointConnection -ResourceId $PepId[0]).Name
+        $Status = (Get-AzPrivateEndpointConnection -ResourceId $PepId[0].Id).PrivateLinkServiceConnectionState.Status
+        $Name = (Get-AzPrivateEndpointConnection -ResourceId $PepId[0].Id).Name
+        Write-Output $($Status + ": " + $Name)
+    }
+    else {
         Write-Output "Nothing to approve $($Name)"
     }
 }
