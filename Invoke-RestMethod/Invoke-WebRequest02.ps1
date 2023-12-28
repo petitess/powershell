1f6bf6d4f8a3763cce4e1d17de3cc955cd4545fe
Karol <93782725+petitess@users.noreply.github.com>
2023-12-28 08:52:15 +0100

    POST dstny.se adc-grupp - login v2

--- Invoke-RestMethod/Invoke-WebRequest02.ps1
+++ Invoke-RestMethod/Invoke-WebRequest02.ps1
@@ -1,29 +1,30 @@
+Connect-AzAccount -Identity
+
 $Day = (Get-Date).DayOfWeek
 $Time = Get-Date -Format "HH:mm"
 Write-Output $($Day.ToString() + " " + $Time.ToString())
 
 ###Opsgenie info
-$ApiOpsgenie = Get-AzKeyVaultSecret -VaultName "kv-b3-aa-prod-01" -Name "ApiKeyOpsgenie" -AsPlainText
+$ApiOpsgenie = Get-AzKeyVaultSecret -VaultName "kv-xxx-aa-prod-01" -Name "ApiKeyOpsgenie" -AsPlainText
 $ScheduleName = 'Beredskap_schedule'
-
 $HeadersOpsgenie = @{
     "Authorization" = "GenieKey $ApiOpsgenie"
     "Content-type"  = "application/json"
 }
-if($Day -eq 'Thursday' -and $Time -lt "08:00") {
-$UrlOpsgenie = "https://api.opsgenie.com/v2/schedules/$ScheduleName/on-calls?scheduleIdentifierType=name"
-$NextOnCall = (Invoke-RestMethod -Method GET -URI $UrlOpsgenie -Headers $HeadersOpsgenie).data.onCallParticipants.name
-}else {
-$UrlOpsgenie = "https://api.opsgenie.com/v2/schedules/$ScheduleName/next-on-calls?scheduleIdentifierType=name"
-$NextOnCall = (Invoke-RestMethod -Method GET -URI $UrlOpsgenie -Headers $HeadersOpsgenie).data.exactNextOnCallRecipients.name
+
+if ($Day -eq 'Thursday' -and $Time -lt "07:00") {
+    Write-Output "Kör inte scriptet"
+}
+else {
+    $UrlOpsgenie = "https://api.opsgenie.com/v2/schedules/$ScheduleName/next-on-calls?scheduleIdentifierType=name"
+    $NextOnCall = (Invoke-RestMethod -Method GET -URI $UrlOpsgenie -Headers $HeadersOpsgenie).data.exactNextOnCallRecipients.name
+    Write-Output $($NextOnCall + " har beredskap")
 }
 
-Write-Output $($NextOnCall + " har beredskap")
-
 ###Info dstny.se
-$ApiKey = Get-AzKeyVaultSecret -VaultName "kv-xxx-prod-01" -Name "ApiKeyDstny" -AsPlainText
+$ApiKey = Get-AzKeyVaultSecret -VaultName "kv-xxx-aa-prod-01" -Name "ApiKeyDstny" -AsPlainText
 $ApiKeyUserId = 'x105'#Karol
-$Domain = 'xxx.se'
+$Domain = 'xxxit.se'
 $Group = '82441'
 $URL = "https://bc.dstny.se/api/user/acd-attendant-group/v1/$Domain/$ApiKeyUserId"
 $headers = @{
@@ -44,33 +45,39 @@ $DstnyUsers | ForEach-Object {
 
 switch ($NextOnCall) {
     'x.sek@xxx.se' {
-        $DstnyUserId = 'x105@xxx.se'
+        $DstnyUserId = 'x105@xxxit.se'
     }
     'x.ljungstrom@xxx.se' {
-        $DstnyUserId = 'x106@xxx.se'
+        $DstnyUserId = 'x106@xxxit.se'
     }
     'x.sjoblom@xxx.se' {
-        $DstnyUserId = 'x117@xxx.se'
+        $DstnyUserId = 'x117@xxxit.se'
     }
     'x.bilger@xxx.se' {
-        $DstnyUserId = 'x115@xxx.se'
+        $DstnyUserId = 'x115@xxxit.se'
     }
-    'x.torntorp@xxx.se' {
-        $DstnyUserId = 'x121@xxx.se'
+    'kim.torntorp@xxx.se' {
+        $DstnyUserId = 'x121@xxxit.se'
     }
     'x.holm@xxx.se' {
-        $DstnyUserId = 'x52@xxx.se'
+        $DstnyUserId = 'x752@xxxit.se'
     }
 }
 
-$URL = "https://bc.dstny.se/api/user/acd-attendant-group/$Domain/$ApiKeyUserId/$Group/$Domain/agents/$($DstnyUserId)?action=login"
-$IR = Invoke-WebRequest -Method POST -Uri $URL -Headers $headers
+if ($null -eq $NextOnCall) {
+    Write-Output "Inget API anrop"
+}
+else {
+    $URL = "https://bc.dstny.se/api/user/acd-attendant-group/$Domain/$ApiKeyUserId/$Group/$Domain/agents/$($DstnyUserId)?action=login"
+    $IR = Invoke-WebRequest -Method POST -Uri $URL -Headers $headers
 
-Write-Output $("Status code: " + $IR.StatusCode)
+    Write-Output $("Status code: " + $IR.StatusCode)
 
-If($IR.StatusCode -eq "200") {
-    Write-Output $($NextOnCall + " inloggad på dsnty")
+    If ($IR.StatusCode -eq "200") {
+        Write-Output $($NextOnCall + " inloggad på dsnty")
 
-}else {
-    Write-Output $("Status code: " + $($IR.StatusCode) + ". Check the script")
+    }
+    else {
+        Write-Output $("Status code: " + $($IR.StatusCode) + ". Check the script")
+    }
 }
