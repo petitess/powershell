e83b1f970c4ce95c7481d6cff4b4f5175bf88f19
Karol <93782725+petitess@users.noreply.github.com>
2024-01-08 15:09:50 +0100

    User tenant permission

--- Azure/New-AzRoleAssignment02.ps1
+++ Azure/New-AzRoleAssignment02.ps1
@@ -8,3 +8,6 @@ Get-AzRoleAssignment -ObjectId $user.Id -Scope "/"
 
 Remove-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Owner' -ObjectId $user.Id
 
+New-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Management Group Contributor' -ObjectId $user.Id
+New-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Management Group Reader' -ObjectId $user.Id
+
