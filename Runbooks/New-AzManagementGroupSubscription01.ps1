64f48efaee48188c7038556927977041b9efd6eb
Karol <93782725+petitess@users.noreply.github.com>
2024-01-08 16:22:41 +0100

    Move subscriptions to new management group

--- Runbooks/New-AzManagementGroupSubscription01.ps1
+++ Runbooks/New-AzManagementGroupSubscription01.ps1
@@ -1,15 +1,27 @@
 param (
     [Parameter(Mandatory)]
+    [Alias('Subscription Name')]
     [string]$SUBNAME,
 
     [Parameter(Mandatory)]
+    [Alias('Management Group Id')]
     [string]$NEWMG
 )
 Connect-AzAccount -Identity
 
-$OldMg = "xxxx-b710-439d-b5bd-xxx"
+<#
+Prerequisite:
+Automation account must be 'Management Group Contributor' and 'Owner at Root Scope('/')
+New-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Management Group Contributor' -ObjectId $ObjectId
+New-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Owner' -ObjectId $ObjectId
+#>
+
+$OldMg = "xxxx-b710-439d-b5bd-xxxx"
 $Sub = Get-AzManagementGroupSubscription -GroupName $OldMg | Where-Object { $_.DisplayName -eq $SUBNAME }
 if ($null -ne $Sub) {
     $SubId = $($Sub.Id).Replace("/providers/Microsoft.Management/managementGroups/$OldMg/subscriptions/", "")
-    New-AzManagementGroupSubscription -GroupName $NEWMG -SubscriptionId $SubId
+    $Result = New-AzManagementGroupSubscription -GroupName $NEWMG -SubscriptionId $SubId
+    Write-Output "$($Result.DisplayName) moved to $NEWMG" 
+}else {
+    Write-Output "Did not find any subscription with this name in Tenant Root Group"
 }
