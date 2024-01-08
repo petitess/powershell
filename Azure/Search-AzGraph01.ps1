c0530254049a837d62c8151e53f6b90a519e0c75
Karol <93782725+petitess@users.noreply.github.com>
2024-01-08 11:32:26 +0100

    Azure Resource Graph Explorer

--- Azure/Search-AzGraph01.ps1
+++ Azure/Search-AzGraph01.ps1
@@ -8,11 +8,25 @@ Search-AzGraph -Query "patchassessmentresources
 security = properties.availablePatchCountByClassification.security,
 critical = properties.availablePatchCountByClassification.critical
 | project resourceGroup, security, critical"
-
-
-
+#
 Search-AzGraph -Query "resources
 |where type == 'microsoft.keyvault/vaults'
 |where properties.publicNetworkAccess == 'Enabled'
 |mv-expand properties.networkAcls.ipRules
 |project kvName = name, kvRule = properties.networkAcls.ipRules"
+#
+$Query = 'securityresources | where type == "microsoft.security/assessments"'
+Search-AzGraph -Query $Query
+#
+$Query = "
+securityresources
+| where name == '.net_6.0.24.0'
+"
+Search-AzGraph -Query $Query | ConvertTo-Json
+#
+$Query = "
+securityresources
+| where name == '.net_6.0.24.0'
+| summarize Count = count() by name
+"
+Search-AzGraph -Query $Query
