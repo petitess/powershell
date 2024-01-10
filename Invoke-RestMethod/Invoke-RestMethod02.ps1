488341cd860d6464956f94ce7e539966cd85d9d8
Karol <93782725+petitess@users.noreply.github.com>
2024-01-10 11:21:10 +0100

    update key vault secret

--- Invoke-RestMethod/Invoke-RestMethod02.ps1
+++ Invoke-RestMethod/Invoke-RestMethod02.ps1
@@ -1,3 +1,30 @@
-$Method = 'GET'
-$Uri = "ase-app-test.azurewebsites.net"
-Invoke-RestMethod -Method $Method -Uri $Uri
+###GET Key valut secret
+$subscriptionId = (Get-AzSubscription -SubscriptionName "sub-b3care-prod-01").Id
+$RessourceGroupName = "rg-infra-prod-sc-01"
+$KvName = "kv-b3care-infra-prod-01"
+$SecretName = "a-sec"
+$ApiVersion = "2023-07-01"
+$URL = "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$RessourceGroupName/providers/Microsoft.KeyVault/vaults/$KvName/secrets/$($SecretName)?api-version=$ApiVersion"
+$headers = @{
+    "Authorization" = "Bearer $((Get-AzAccessToken).Token)"
+    "Content-type"  = "application/json"
+}
+(Invoke-RestMethod -Method GET -URI $URL -Headers $headers) | ConvertTo-Json
+
+###PUT Key valut secret
+$subscriptionId = (Get-AzSubscription -SubscriptionName "sub-b3care-prod-01").Id
+$RessourceGroupName = "rg-infra-prod-sc-01"
+$KvName = "kv-b3care-infra-prod-01"
+$SecretName = "a-sec"
+$ApiVersion = "2023-07-01"
+$URL = "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$RessourceGroupName/providers/Microsoft.KeyVault/vaults/$KvName/secrets/$($SecretName)?api-version=$ApiVersion"
+$headers = @{
+    "Authorization" = "Bearer $((Get-AzAccessToken).Token)"
+    "Content-type"  = "application/json"
+}
+$Body = ConvertTo-Json @{
+    properties = @{
+        value= "booom"
+    }
+}
+Invoke-RestMethod -Method PUT -URI $URL -Headers $headers -Body $Body
