0c6fce785a492f174ffe1372c8cfd0d4084af5b1
Karol <93782725+petitess@users.noreply.github.com>
2024-01-30 13:04:04 +0100

    RegEx to extract text between delimiters

--- regex01.ps1
+++ regex01.ps1
@@ -4,3 +4,7 @@ $secondString = '</title>'
 $pattern = "$firstString(.*?)$secondString"
 $result = [regex]::Match($htmlString,$pattern).Groups[1].Value
 $result
+
+##extract text between []
+$existingMsg = "[532454] [CUSTOMER] ALERT TEST"
+[regex]::Matches($M , '(?<=\[)[^]]+(?=\])').Value[0]
