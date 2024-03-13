fe867784998898489779c80f2a02842e8ed62078
Karol <93782725+petitess@users.noreply.github.com>
2024-03-13 17:47:44 +0100

    password as plain text

--- Get-Credential01.ps1
+++ Get-Credential01.ps1
@@ -1,4 +1,8 @@
-$User = "localhost\stavdprofilesdev02"
-$PWord = "xcYv4L0SHtLoU0QieUhhpbdAvBHtBfZnCnVhLA1Nmr1RuhwGqh277BqG+AStJVnLNw=="
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential($User, $PWord)
-$Cred = Get-Credential -Credential $Credential
+$appid = "xxxxx-f137-4e11-b18e-6db395c662aa"
+$tenantid = 'bcee8bc2-b710-439d-b5bd-f5b83846ddee'
+$secret = ConvertTo-SecureString -String 'xxxxxx-ar2BRfWQlj4iWElQANwX~m4R7-MaGcr1' -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $appid, $secret
+$ClientSecretCredential = Get-Credential -Credential $Credential
+# Enter client_secret in the password prompt.
+Connect-MgGraph -TenantId $tenantid -ClientSecretCredential $ClientSecretCredential
+Get-MgContext
