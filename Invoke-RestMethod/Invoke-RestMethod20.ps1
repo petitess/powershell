374478774465d98b73d1a0613fac8a59dea0df0e
Karol <93782725+petitess@users.noreply.github.com>
2024-05-02 12:51:12 +0200

    Devops - Elasticpools

--- Invoke-RestMethod/Invoke-RestMethod20.ps1
+++ Invoke-RestMethod/Invoke-RestMethod20.ps1
@@ -1,21 +1,27 @@
 ###https://learn.microsoft.com/en-us/rest/api/azure/devops/distributedtask/elasticpools/update?view=azure-devops-rest-6.1
 
-$Token = az account get-access-token --query accessToken --output tsv
-$URL = "https://dev.azure.com/ssgse/_apis/distributedtask/elasticpools?api-version=6.1-preview.1"
+$Time = Get-Date -Format "HH"
+
+Get-Date -Format "HH:mm K"
+
+$Token = $(az account get-access-token --query accessToken --output tsv)
+$URL = "https://dev.azure.com/xxxse/_apis/distributedtask/elasticpools?api-version=6.1-preview.1"
 $headers = @{
     "Authorization" = "Bearer $Token"
     "Content-type"  = "application/json"
 }
-(Invoke-RestMethod -Method GET -URI $URL -Headers $headers).value | Where-Object {$_.azureId -like "*vmss-infra-devops-dev-we-01"} | ConvertTo-Json 
+$Pools = (Invoke-RestMethod -Method GET -URI $URL -Headers $headers).value | Where-Object { $_.azureId -like "*vmss-infra-devops*" }
 
+$Pools | ForEach-Object {
 
-$Token = az account get-access-token --query accessToken --output tsv
-$URL = "https://dev.azure.com/ssgse/_apis/distributedtask/elasticpools/46?api-version=6.1-preview.1"
-$headers = @{
-    "Authorization" = "Bearer $Token"
-    "Content-type" = "application/json"
+    $URL = "https://dev.azure.com/xxxse/_apis/distributedtask/elasticpools/$($_.poolId)?api-version=6.1-preview.1"
+    $headers = @{
+        "Authorization" = "Bearer $Token"
+        "Content-type"  = "application/json"
+    }
+    $Body = ConvertTo-Json @{
+        desiredIdle = $Time -gt 17 ? 0 : 1
+    }
+    Write-Output "Number of agents to keep on standby: $($Time -gt 17 ?  0 : 1) " 
+    Invoke-RestMethod -Method PATCH -URI $URL -Headers $headers -Body $Body
 }
-$Body = ConvertTo-Json @{
-    desiredIdle    = 3
-}
-Invoke-RestMethod -Method PATCH -URI $URL -Headers $headers -Body $Body
