728c07d469b84cf4e551c092fbd9a849c6f33ac7
Karol <93782725+petitess@users.noreply.github.com>
2025-03-21 09:58:39 +0100

    authenticate with github app, jwt+gh token

--- Invoke-RestMethod/Invoke-RestMethod07.ps1
+++ Invoke-RestMethod/Invoke-RestMethod07.ps1
@@ -1,36 +1,71 @@
-###UPDATE
-$subscriptionId = (Get-AzSubscription -SubscriptionName "sub-default-01").Id
-$apiversion = "2021-04-01"
-$URL = "https://management.azure.com/subscriptions/$($subscriptionId)/providers/Microsoft.Resources/tags/default?api-version=$apiversion"
+#!/usr/bin/env pwsh
+#GET jwt token
+$client_id = "Iv23licXTSHNzdyxxxxx"
+$private_key_path = "C:\Users\K1611553\Downloads\karol-app.2025-03-21.private-key.pem"
 
+$header = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes((ConvertTo-Json -InputObject @{
+  alg = "RS256"
+  typ = "JWT"
+}))).TrimEnd('=').Replace('+', '-').Replace('/', '_');
+
+$payload = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes((ConvertTo-Json -InputObject @{
+  iat = [System.DateTimeOffset]::UtcNow.AddSeconds(-10).ToUnixTimeSeconds()
+  exp = [System.DateTimeOffset]::UtcNow.AddMinutes(10).ToUnixTimeSeconds()
+   iss = $client_id 
+}))).TrimEnd('=').Replace('+', '-').Replace('/', '_');
+
+$rsa = [System.Security.Cryptography.RSA]::Create()
+$rsa.ImportFromPem((Get-Content $private_key_path -Raw))
+
+$signature = [Convert]::ToBase64String($rsa.SignData([System.Text.Encoding]::UTF8.GetBytes("$header.$payload"), [System.Security.Cryptography.HashAlgorithmName]::SHA256, [System.Security.Cryptography.RSASignaturePadding]::Pkcs1)).TrimEnd('=').Replace('+', '-').Replace('/', '_')
+$jwt = "$header.$payload.$signature"
+Write-Host $jwt
+#GET installation id
+$JwtToken = $jwt
+$URL = "https://api.github.com/app/installations"
 $headers = @{
-    "Authorization" = "Bearer $((Get-AzAccessToken).Token)"
-    "Content-type"  = "application/json"
+    "Authorization"        = "Bearer $JwtToken"
+    "Accept"               = "application/vnd.github+json"
+    "X-GitHub-Api-Version" = "2022-11-28"
+}
+$Ins = Invoke-RestMethod -Method GET -URI $URL -Headers $headers
+# GET github token
+# $JwtToken = ""
+$Id = $Ins.id
+$URL = "https://api.github.com/app/installations/$Id/access_tokens"
+$headers = @{
+    "Authorization"        = "Bearer $JwtToken"
+    "Accept"               = "application/vnd.github+json"
+    "X-GitHub-Api-Version" = "2022-11-28"
 }
 
-$bodyNewRessourceGroup = @"
-{
-    "operation":"merge",
-    "properties": {
-      "tags": {
-        "SYSTEM" : "$(Get-Date)"
-      }
+$GhToken = Invoke-RestMethod -Method POST -URI $URL -Headers $headers
+$GhToken.token
+#GET repos
+$Token = $GhToken.token
+$URL = "https://api.github.com/orgs/045-SGEC-APPLICATIONS/repos"
+$headers = @{
+    "Authorization"        = "Bearer $Token"
+    "Accept"               = "application/vnd.github+json"
+    "X-GitHub-Api-Version" = "2022-11-28"
+    "User-Agent"           = "user"
+}
+$Repos = Invoke-RestMethod -Method GET -URI $URL -Headers $headers
+$Repos.name
+#GET workflow runs
+$Repos.name | ForEach-Object {
+    $Repo = $_
+    $Token = $GhToken.token
+    $URL = "https://api.github.com/repos/045-SGEC-APPLICATIONS/$_/actions/runs"
+    $headers = @{
+        "Authorization"        = "Bearer $Token"
+        "Accept"               = "application/vnd.github+json"
+        "X-GitHub-Api-Version" = "2022-11-28"
+        "User-Agent"           = "user"
     }
-  }
-"@
-
-Invoke-RestMethod -Method PATCH -URI $URL -body $bodyNewRessourceGroup -Headers $headers
-
-###GET
-$subscriptionId = (Get-AzSubscription -SubscriptionName "sub-default-01").Id
-$apiversion = "2021-04-01"
-$URL = "https://management.azure.com/subscriptions/$($subscriptionId)/providers/Microsoft.Resources/tags/default?api-version=$apiversion"
-
-$headers = @{
-    "Authorization" = "Bearer $((Get-AzAccessToken).Token)"
-    "Content-type"  = "application/json"
+    $Runs = Invoke-RestMethod -Method GET -URI $URL -Headers $headers
+    $Runs.workflow_runs | Where-Object { $_.event -eq "schedule" -and $_.conclusion -ne "success" } | Select-Object name, conclusion, created_at, @{Name = "repo"; Expression = {$Repo}} | Sort-Object name -Unique
 }
 
-Invoke-RestMethod -Method GET -URI $URL -Headers $headers | ConvertTo-Json
 
-(Invoke-RestMethod -Method GET -URI $URL -Headers $headers).properties.tags 
+
