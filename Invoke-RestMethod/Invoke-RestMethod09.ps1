b722499aa33a30d96a381e716d7facd51c85fe95
Karol <93782725+petitess@users.noreply.github.com>
2025-05-27 11:12:28 +0200

    Get entra group members graph

--- Invoke-RestMethod/Invoke-RestMethod09.ps1
+++ Invoke-RestMethod/Invoke-RestMethod09.ps1
@@ -1,63 +1,7 @@
-###GET
-$Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMDIyIiwibmJmIjoxNzAyNDg3MzQ5LCJleHAiOjE3MDI2NjAxNDksImlhdCI6MTcwMjQ4NzM0OX0.-N_ppHZF0oqjJ8tfl3DvrrjanVerWOKQpO1VuvBavOk"
-$URL = "http://localhost:5000/Post/GetMyPosts"
+$Token = az account get-access-token --resource "https://graph.microsoft.com" --query "accessToken" -o tsv
+$URL = "https://graph.microsoft.com/v1.0/groups/1c354157-c1cd-4d1e-8a73-d0b70a566ea9/members"
 $headers = @{
     "Authorization" = "Bearer $Token"
     "Content-type"  = "application/json"
 }
-Invoke-RestMethod -Method GET -URI $URL -Headers $headers
-##POST
-$Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMDIyIiwibmJmIjoxNzAyNDg3MzQ5LCJleHAiOjE3MDI2NjAxNDksImlhdCI6MTcwMjQ4NzM0OX0.-N_ppHZF0oqjJ8tfl3DvrrjanVerWOKQpO1VuvBavOk"
-$URL = "http://localhost:5000/Post/AddPost"
-$headers = @{
-    
-    "Authorization" = "Bearer $Token"
-    "Content-type"  = "application/json"
-}
-$body = @"
-    {
-        "postTitle": "SMARTPHONE",
-        "postContent": "SAMSUNG GALAXY"
-    }
-"@
-Invoke-RestMethod -Method POST -URI $URL -Headers $headers -body $body
-##PUT
-$Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMDIyIiwibmJmIjoxNzAyNDg3MzQ5LCJleHAiOjE3MDI2NjAxNDksImlhdCI6MTcwMjQ4NzM0OX0.-N_ppHZF0oqjJ8tfl3DvrrjanVerWOKQpO1VuvBavOk"
-$URL = "http://localhost:5000/Post/UpsertPost"
-$headers = @{
-    
-    "Authorization" = "Bearer $Token"
-    "Content-type"  = "application/json"
-}
-$body = @"
-    {
-        "postTitle": "COMPUTER",
-        "postContent": "ThinkSmart"
-    }
-"@
-Invoke-RestMethod -Method PUT -URI $URL -Headers $headers -body $body
-##PUT
-$Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMDIyIiwibmJmIjoxNzAyNDg3MzQ5LCJleHAiOjE3MDI2NjAxNDksImlhdCI6MTcwMjQ4NzM0OX0.-N_ppHZF0oqjJ8tfl3DvrrjanVerWOKQpO1VuvBavOk"
-$URL = "http://localhost:5000/Post/UpsertPost"
-$headers = @{
-    
-    "Authorization" = "Bearer $Token"
-    "Content-type"  = "application/json"
-}
-$body = @"
-    {
-        "postId" : "7",
-        "postTitle": "COMPUTER",
-        "postContent": "Lenovo ThinkSmart"
-    }
-"@
-Invoke-RestMethod -Method PUT -URI $URL -Headers $headers -body $body
-##DELETE
-$Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMDIyIiwibmJmIjoxNzAyNDg3MzQ5LCJleHAiOjE3MDI2NjAxNDksImlhdCI6MTcwMjQ4NzM0OX0.-N_ppHZF0oqjJ8tfl3DvrrjanVerWOKQpO1VuvBavOk"
-$URL = "http://localhost:5000/Post/DeletePost/9"
-$headers = @{
-    
-    "Authorization" = "Bearer $Token"
-    "Content-type"  = "application/json"
-}
-Invoke-RestMethod -Method Delete -URI $URL -Headers $headers
+(Invoke-RestMethod -Method GET -URI $URL -Headers $headers).value
