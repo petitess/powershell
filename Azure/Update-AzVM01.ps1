3e654bae2d107d8dab8b0a05ceccec921f92dc7f
Karol <93782725+petitess@users.noreply.github.com>
2025-11-20 05:43:00 +0100

    update VM size

--- Azure/Update-AzVM01.ps1
+++ Azure/Update-AzVM01.ps1
@@ -6,10 +6,12 @@ $VMs = Get-AzVM -Status | Where-Object { $_.Tags.Keys -eq $TagKey -and $_.Tags.V
 
 $VMs | ForEach-Object {
         if ($_.PowerState -eq "VM running" ) {
+                $_.OSProfile = $null
                 $_.HardwareProfile.VmSize = $VMsize
                 Update-AzVM -VM $_ -ResourceGroupName $_.ResourceGroupName
                 Write-Output "Updated size: $($_.Name)"
         }if ($_.PowerState -eq "VM deallocated" ) {
+                $_.OSProfile = $null
                 $_.HardwareProfile.VmSize = $VMsize
                 Update-AzVM -VM $_ -ResourceGroupName $_.ResourceGroupName
                 Write-Output "Updated size: $($_.Name)"
