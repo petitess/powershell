325799069bf188eee014372f0b203096f84e8f61
Karol <93782725+petitess@users.noreply.github.com>
2026-07-07 15:01:58 +0200

    Delete MicrosoftGraph directory

--- MicrosoftGraph/Connect-MgGraph01.ps1
+++
@@ -1,7 +0,0 @@
-Connect-MgGraph -TenantId "xxx" -Scopes 'User.Read.All'
-
-Connect-MgGraph -Scopes "User.Read.All","MailboxSettings.Read"
-
-Get-MgContext
-
-Get-MgUser -All
--- MicrosoftGraph/Connect-MgGraph02.ps1
+++
@@ -1,3 +0,0 @@
-#https://learn.microsoft.com/en-us/graph/permissions-reference
-Connect-MgGraph -Scopes "User.Read.All" -ClientId "abc" -TenantId "abc"
-
